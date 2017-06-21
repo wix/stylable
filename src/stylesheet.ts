cafb82ecaa95e47337eff607c0389993715f35d1
Barak Igal <baraki@lan.wixpress.com>
2017-06-21 14:37:51 +0300

    added imports

--- src/stylesheet.ts
+++ src/stylesheet.ts
@@ -1,5 +1,12 @@
 import { Pojo } from './index.d';
-import { hasOwn, parseSelector, objectifyCSS, isOnlyElementOrClassSelector, SelectorAstNode, traverseNode } from './parser';
+import {
+    createSimpleSelectorChecker,
+    hasOwn,
+    objectifyCSS,
+    parseSelector,
+    PseudoSelectorAstNode,
+    traverseNode,
+} from './parser';
 
 const postjs = require("postcss-js");
 const postcss = require("postcss");
@@ -15,14 +22,50 @@ export class InMemoryContext {
 }
 
 
+export interface CSSImportRaw {
+    SbDefault: string;
+    SbNamed: string;
+    [key: string]: string;
+}
+
+export class Import {
+    static fromRawCSS(SbFrom: string, cssImportDef: CSSImportRaw){
+        
+        const namedMap: Pojo<string> = {};
+
+        if(cssImportDef["SbNamed"]) {
+            cssImportDef["SbNamed"].split(',').forEach((name)=>{
+                const parts = name.trim().split(/\s+as\s+/);
+                if(parts.length === 1){
+                    namedMap[parts[0]] = parts[0];
+                } else if(parts.length === 2){
+                    namedMap[parts[0]] = parts[1];
+                }
+            })
+        }
+
+        for(var key in cssImportDef){
+            const match = key.match(/^SbNamed(.+)/);
+            if(match){
+                namedMap[match[1]] = cssImportDef[key];
+            }
+        }
+        
+        return new Import(SbFrom.slice(1, -1), cssImportDef.SbDefault, namedMap);   
+    }
+    constructor(public SbFrom: string, public SbDefault: string = "", public SbNamed: Pojo<string> = {}){}
+}
+                 
 export class Stylesheet {
     cssDefinition: any;
     classes: Pojo<string>;
     typedClasses: Pojo<TypedClass>;
+    imports: Import[];
     constructor(cssDefinition: any) {
         this.cssDefinition = cssDefinition;
         this.classes = {};
         this.typedClasses = {};
+        this.imports = [];
         this.process();
     }
     static fromCSS(css: string) {
@@ -39,17 +82,23 @@ export class Stylesheet {
     private process() {
         Object.keys(this.cssDefinition).forEach((selector: string) => {
             const ast = parseSelector(selector);
-            this.addTypedClass(selector, ast);
+            const checker = createSimpleSelectorChecker();
+            let isSimpleSelector = true;
             traverseNode(ast, (node) => {
-                if (node.type === 'class') {
+                if(!checker(node)){ isSimpleSelector = false; }
+                const { type, name } = node;
+                if(type === "pseudo-class" && name === 'import'){
+                    const {content} = node as PseudoSelectorAstNode;
+                    this.imports.push(Import.fromRawCSS(content || this.cssDefinition[selector]['SbFrom'], this.cssDefinition[selector]));
+                } else if (node.type === 'class') {
                     this.addClassNameMapping(node.name);
                 }
-            });
+            });            
+            this.addTypedClass(selector, isSimpleSelector);
         });
     }
-    private addTypedClass(selector: string, ast: SelectorAstNode) {
+    private addTypedClass(selector: string, isSimpleSelector: boolean) {
         const rules: Pojo<string> = this.cssDefinition[selector];
-        const isSimpleSelector = isOnlyElementOrClassSelector(ast);
         const name = selector.replace('.', '');
         if (hasOwn(rules, 'SbRoot')) {
             if (isSimpleSelector) {
