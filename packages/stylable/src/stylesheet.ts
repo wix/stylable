948584b6ceec77cc9581bbc8672f4874b32cf727
Barak Igal <baraki@lan.wixpress.com>
2017-06-21 12:34:09 +0300

    refactor

--- packages/stylable/src/stylesheet.ts
+++ packages/stylable/src/stylesheet.ts
@@ -1,19 +1,14 @@
-import { Pojo } from "./index.d";
+import { Pojo } from './index.d';
+import { hasOwn, parseSelector, objectifyCSS, isOnlyElementOrClassSelector, SelectorAstNode, traverseNode } from './parser';
+
 const postjs = require("postcss-js");
 const postcss = require("postcss");
 
 const processor = postcss()
 
 export interface TypedClass {
-    SbRoot: boolean
+    SbRoot: boolean;
 }
-
-export interface Meta {
-    cssDefinition: any;
-    typedClasses: Pojo<TypedClass>;
-    classes: Pojo<string>;
-}
-
 
 export class InMemoryContext {
     constructor(public buffer: string[] = []) { }
@@ -21,21 +16,56 @@ export class InMemoryContext {
 
 
 export class Stylesheet {
+    cssDefinition: any;
     classes: Pojo<string>;
-    constructor(public meta: Partial<Meta>) {
-        this.classes = meta.classes || {};
+    typedClasses: Pojo<TypedClass>;
+    constructor(cssDefinition: any) {
+        this.cssDefinition = cssDefinition;
+        this.classes = {};
+        this.typedClasses = {};
+        this.process();
+    }
+    static fromCSS(css: string) {
+        return new Stylesheet(objectifyCSS(css));
     }
     generate(ctx: InMemoryContext) {
-        this.meta.cssDefinition && Object.keys(this.meta.cssDefinition).forEach((selector) => {
-            if (Object.keys(this.meta.cssDefinition[selector]).length) {
-                var result = processor.process({ [selector]: this.meta.cssDefinition[selector] }, { parser: postjs });
+        Object.keys(this.cssDefinition).forEach((selector) => {
+            if (Object.keys(this.cssDefinition[selector]).length) {
+                var result = processor.process({ [selector]: this.cssDefinition[selector] }, { parser: postjs });
                 result.css && ctx.buffer.push(result.css);
             }
         });
     }
-    
-}
+    private process() {
+        Object.keys(this.cssDefinition).forEach((selector: string) => {
+            const ast = parseSelector(selector);
+            this.addTypedClass(selector, ast);
+            traverseNode(ast, (node) => {
+                if (node.type === 'class') {
+                    this.addClassNameMapping(node.name);
+                }
+            });
+        });
+    }
+    private addTypedClass(selector: string, ast: SelectorAstNode) {
+        const rules: Pojo<string> = this.cssDefinition[selector];
+        const isSimpleSelector = isOnlyElementOrClassSelector(ast);
+        const name = selector.replace('.', '');
+        if (hasOwn(rules, 'SbRoot')) {
+            if (isSimpleSelector) {
+                this.typedClasses[name] = {
+                    SbRoot: rules['SbRoot'] === 'false' ? false : true
+                };
+            } else {
+                throw new Error('-sb-root on complex selector: ' + selector);
+            }
+        }
+    }
 
+    private addClassNameMapping(originalName: string, mappedName: string = originalName) {
+        this.classes[originalName] = mappedName;
+    }
+}
 
 
 
--- packages/stylable/tests/stylesheet.spec.ts
+++ packages/stylable/tests/stylesheet.spec.ts
@@ -3,6 +3,197 @@ import { expect } from "chai";
 
 describe('Stylesheet', function () {
 
+
+    describe('create', function () {
+
+        it('create css in js definition', function () {
+
+            var sheet = Stylesheet.fromCSS(`
+                .container {
+                    color: red;
+                }
+            `);
+
+            expect(sheet.cssDefinition).to.eql({
+                ".container": { color: "red" }
+            });
+
+        });
+
+        it('create a stylesheet from css', function () {
+
+            var sheet = Stylesheet.fromCSS(`
+                .container { }
+            `)
+
+            expect(sheet.classes).to.eql({
+                container: "container"
+            });
+
+        });
+
+        it('create a stylesheet from css with multiple selectors', function () {
+
+            var sheet = Stylesheet.fromCSS(`
+                .container { }
+                .image { }
+            `)
+
+            expect(sheet.classes).to.eql({
+                container: "container",
+                image: "image"
+            });
+
+        });
+
+        it('create a stylesheet from css with nested selector', function () {
+
+            var sheet = Stylesheet.fromCSS(`
+                .container .image { } 
+            `)
+
+            expect(sheet.classes).to.eql({
+                container: "container",
+                image: "image"
+            });
+
+        });
+
+
+        it('create a stylesheet from css with multiple selectors in the same declaration', function () {
+
+            var sheet = Stylesheet.fromCSS(`
+                .container, .wrapper .image { } 
+            `)
+
+            expect(sheet.classes).to.eql({
+                container: "container",
+                image: "image",
+                wrapper: "wrapper"
+            });
+
+        });
+
+    });
+
+    describe('process', function () {
+
+        it('create typedClasses definitions', function () {
+
+            var sheet = Stylesheet.fromCSS(`
+                .container {
+                    -sb-root: true;
+                    color: red;
+                }
+            `);
+
+            expect(sheet.typedClasses).to.eql({
+                container: {
+                    SbRoot: true
+                }
+            });
+
+        });
+
+        it('throw when -sb-root used in complex selector', function () {
+
+            expect(function () {
+                Stylesheet.fromCSS(`
+                    .container[attr] {
+                        -sb-root: true;
+                        color: red;
+                    }
+                `);
+            }).throw('-sb-root on complex selector: .container[attr]');
+
+        });
+
+        xit('create import definitions', function () {
+            /*
+                
+                :import("./path/to/thing"){
+                    -sb-default: Name;
+                    -sb-named: Button as Btn, Icon;
+                    -sb-named-Name: MyName;
+                }
+
+                :import{
+                    -sb-from: "./path/to/thing";
+                    -sb-default: Name;
+                    -sb-named: Button as Btn, Icon;
+                    -sb-named-Name: MyName;
+                }
+
+             */
+            var sheet = Stylesheet.fromCSS(`
+                
+            `);
+
+            expect(sheet.typedClasses).to.eql({
+                container: {
+                    SbType: {
+                        from: "./button",
+                        default: "Button",
+                        named: {}
+                    }
+                }
+            });
+
+        });
+
+        it('with empty css', function () {
+            const sheet = Stylesheet.fromCSS(``);
+
+            expect(sheet.classes).to.eql({});
+            expect(sheet.typedClasses).to.eql({});
+        });
+
+        it('with typed class -sb-root true', function () {
+
+            const sheet = Stylesheet.fromCSS(`
+                .container {
+                    -sb-root: true;
+                }
+            `);
+
+            expect(sheet.typedClasses).to.eql({
+                container: {
+                    SbRoot: true
+                }
+            });
+
+        });
+
+        it('with typed class -sb-root ANY_VALUE that is not "false"', function () {
+            const sheet = Stylesheet.fromCSS(`
+                .container {
+                    -sb-root: ANY_VALUE;
+                }
+            `);
+
+            expect(sheet.typedClasses).to.eql({
+                container: {
+                    SbRoot: true
+                }
+            })
+        });
+
+        it('with typed class -sb-root is false', function () {
+            const sheet = Stylesheet.fromCSS(`
+                .container {
+                    -sb-root: false;
+                }
+            `);
+
+            expect(sheet.typedClasses).to.eql({
+                container: {
+                    SbRoot: false
+                }
+            })
+        });
+
+    });
+
     describe('generate', function () {
 
         let ctx: InMemoryContext;
@@ -15,34 +206,29 @@ describe('Stylesheet', function () {
             const stylesheet = new Stylesheet({});
             stylesheet.generate(ctx);
             expect(ctx.buffer).to.eql([]);
-
         });
 
         it('generate with single rule', function () {
-            const cssDefinition = {
+            const stylesheet = new Stylesheet({
                 ".container": { color: "black" }
-            };
-            const stylesheet = new Stylesheet({ cssDefinition });
+            });
             stylesheet.generate(ctx);
             expect(ctx.buffer).to.eql([".container {\n    color: black\n}"]);
-
         });
 
         it('generate with multiple rules', function () {
-            const cssDefinition = {
+            const stylesheet = new Stylesheet({
                 ".container": { color: "black", background: "white" }
-            };
-            const stylesheet = new Stylesheet({ cssDefinition });
+            });
             stylesheet.generate(ctx);
             expect(ctx.buffer).to.eql([".container {\n    color: black;\n    background: white\n}"]);
         });
 
         it('generate with multiple selectors', function () {
-            const cssDefinition = {
+            const stylesheet = new Stylesheet({
                 ".container": { color: "black" },
                 ".wrapper": { background: "white" }
-            };
-            const stylesheet = new Stylesheet({ cssDefinition });
+            });
             stylesheet.generate(ctx);
             expect(ctx.buffer).to.eql([
                 ".container {\n    color: black\n}",
