bea77a718b9c2d8ee643f72a9ee58cfc799a949d
Barak Igal <baraki@lan.wixpress.com>
2017-06-21 15:46:37 +0300

    added SbStates and SbType basic support

--- packages/stylable/src/stylesheet.ts
+++ packages/stylable/src/stylesheet.ts
@@ -8,6 +8,7 @@ import {
     traverseNode,
 } from './parser';
 
+const kebab = require("kebab-case");
 const postjs = require("postcss-js");
 const postcss = require("postcss");
 
@@ -15,6 +16,8 @@ const processor = postcss()
 
 export interface TypedClass {
     SbRoot: boolean;
+    SbStates: string[];
+    SbType: string;
 }
 
 export class InMemoryContext {
@@ -28,34 +31,47 @@ export interface CSSImportRaw {
     [key: string]: string;
 }
 
+
 export class Import {
-    static fromRawCSS(SbFrom: string, cssImportDef: CSSImportRaw){
-        
+    static fromRawCSS(SbFrom: string, cssImportDef: CSSImportRaw) {
+
         const namedMap: Pojo<string> = {};
 
-        if(cssImportDef["SbNamed"]) {
-            cssImportDef["SbNamed"].split(',').forEach((name)=>{
+        if (cssImportDef["SbNamed"]) {
+            cssImportDef["SbNamed"].split(',').forEach((name) => {
                 const parts = name.trim().split(/\s+as\s+/);
-                if(parts.length === 1){
+                if (parts.length === 1) {
                     namedMap[parts[0]] = parts[0];
-                } else if(parts.length === 2){
+                } else if (parts.length === 2) {
                     namedMap[parts[0]] = parts[1];
                 }
             })
         }
 
-        for(var key in cssImportDef){
+        for (var key in cssImportDef) {
             const match = key.match(/^SbNamed(.+)/);
-            if(match){
+            if (match) {
                 namedMap[match[1]] = cssImportDef[key];
             }
         }
-        
-        return new Import(SbFrom.slice(1, -1), cssImportDef.SbDefault, namedMap);   
+
+        return new Import(SbFrom.slice(1, -1), cssImportDef.SbDefault, namedMap);
     }
-    constructor(public SbFrom: string, public SbDefault: string = "", public SbNamed: Pojo<string> = {}){}
+    constructor(public SbFrom: string, public SbDefault: string = "", public SbNamed: Pojo<string> = {}) { }
 }
-                 
+
+const SBTypes = {
+    SbRoot: (value: string) => {
+        return value === 'false' ? false : true
+    },
+    SbStates: (value: string) => {
+        return value ? value.split(',').map((state) => state.trim()) : [];
+    },
+    SbType: (value: string) => {
+        return value ? value.trim() : "";
+    }
+}
+
 export class Stylesheet {
     cssDefinition: any;
     classes: Pojo<string>;
@@ -84,33 +100,39 @@ export class Stylesheet {
             const ast = parseSelector(selector);
             const checker = createSimpleSelectorChecker();
             let isSimpleSelector = true;
+            debugger;
             traverseNode(ast, (node) => {
-                if(!checker(node)){ isSimpleSelector = false; }
+                if (!checker(node)) { isSimpleSelector = false; }
                 const { type, name } = node;
-                if(type === "pseudo-class" && name === 'import'){
-                    const {content} = node as PseudoSelectorAstNode;
+                if (type === "pseudo-class" && name === 'import') {
+                    const { content } = node as PseudoSelectorAstNode;
                     this.imports.push(Import.fromRawCSS(content || this.cssDefinition[selector]['SbFrom'], this.cssDefinition[selector]));
                 } else if (node.type === 'class') {
                     this.addClassNameMapping(node.name);
                 }
-            });            
+            });
             this.addTypedClass(selector, isSimpleSelector);
         });
     }
     private addTypedClass(selector: string, isSimpleSelector: boolean) {
         const rules: Pojo<string> = this.cssDefinition[selector];
-        const name = selector.replace('.', '');
-        if (hasOwn(rules, 'SbRoot')) {
+        this.addTypedRule(rules, isSimpleSelector, selector, 'SbRoot');
+        this.addTypedRule(rules, isSimpleSelector, selector, 'SbStates');
+        this.addTypedRule(rules, isSimpleSelector, selector, 'SbType');
+    }
+    private addTypedRule(rules: any, isSimpleSelector: boolean, selector: string, rule: keyof typeof SBTypes) {
+        if (hasOwn(rules, rule)) {
             if (isSimpleSelector) {
+                const name = selector.replace('.', '');
                 this.typedClasses[name] = {
-                    SbRoot: rules['SbRoot'] === 'false' ? false : true
+                    ...this.typedClasses[name],
+                    [rule]: SBTypes[rule](rules[rule])
                 };
             } else {
-                throw new Error('-sb-root on complex selector: ' + selector);
+                throw new Error(kebab(rule) + ' on complex selector: ' + selector);
             }
         }
     }
-
     private addClassNameMapping(originalName: string, mappedName: string = originalName) {
         this.classes[originalName] = mappedName;
     }
