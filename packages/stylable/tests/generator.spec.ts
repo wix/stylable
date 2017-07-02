449b085a259a523205f5eb010732e52dbbbdbb50
Barak Igal <baraki@lan.wixpress.com>
2017-07-02 14:11:28 +0300

    handle unknown pseudo-element

--- packages/stylable/tests/generator.spec.ts
+++ packages/stylable/tests/generator.spec.ts
@@ -1,11 +1,12 @@
-import { Generator } from '../src/generator';
+import { Generator, Mode } from '../src/generator';
+import { SelectorAstNode } from '../src/parser';
 import { Stylesheet } from '../src/stylesheet';
 import { expect } from "chai";
 
 
 describe('Generator', function () {
 
-    describe('generator.add', function () {
+    describe('generator.addEntry', function () {
 
         let generator: Generator;
 
@@ -47,7 +48,7 @@ describe('Generator', function () {
             ]);
         });
 
-        
+
         it('generate dose not add the same sheet twice', function () {
             const stylesheet = new Stylesheet({
                 ".container": { color: "black" },
@@ -67,35 +68,83 @@ describe('Generator', function () {
         let generator: Generator;
 
         beforeEach(() => {
-            generator = new Generator({namespaceDivider: "__THE_GREAT_DIVIDER__"});
+            generator = new Generator({ namespaceDivider: "__THE_GREAT_DIVIDER__" });
         });
 
 
         it('generate scoped selector', function () {
-            
+
             const stylesheet = new Stylesheet({
-                ".container": { }
+                ".container": {}
             }, 'TheNameSpace');
-            
+
             generator.addEntry(stylesheet);
-            
+
             expect(generator.buffer[0]).to.eql('.TheNameSpace__THE_GREAT_DIVIDER__container {}');
 
         });
 
         it('generate scoped selector with multiple classes', function () {
 
-            
+
             const stylesheet = new Stylesheet({
-                ".container .img": { }
+                ".container .img": {}
             }, 'TheNameSpace');
-            
+
             generator.addEntry(stylesheet);
-            
+
             expect(generator.buffer[0]).to.eql('.TheNameSpace__THE_GREAT_DIVIDER__container .TheNameSpace__THE_GREAT_DIVIDER__img {}');
 
         });
 
+    });
+
+    describe('Generator.handlePseudoElement', function () {
+
+        it('keep unknown pseudo-element', function () {
+            const generator = new Generator({});
+            const stylesheet = new Stylesheet({}, 'NS');
+            const node: SelectorAstNode = {
+                name: 'myelement',
+                type: 'pseudo-element',
+                nodes: []
+            };
+            const resolved = generator.handlePseudoElement(stylesheet, node, 'myelement');
+            expect(node).to.contain({ name: 'myelement', type: 'pseudo-element' });
+            expect(node.name).to.equal('myelement');
+            expect(resolved).to.equal(stylesheet);
+        });
+
+
+        it('replace known pseudo-elements', function () {
+            const generator = new Generator({});
+            
+            const stylesheet = new Stylesheet({
+                "myelement": {}
+            }, 'NS');
+
+            const node: SelectorAstNode = {
+                name: 'myelement',
+                type: 'pseudo-element',
+                nodes: []
+            };
+            const resolved = generator.handlePseudoElement(stylesheet, node, 'myelement');
+            expect(node).to.contain({ name: 'myelement', type: 'pseudo-element' });
+            expect(node.name).to.equal('myelement');
+            expect(resolved).to.equal(stylesheet);
+        });
+
+    });
+
+    describe('remove empty selectors on production mode', function () {
+        it('generate empty', function () {
+            const generator = new Generator({ mode: Mode.PROD });
+            const stylesheet = new Stylesheet({
+                ".container": {}
+            });
+            generator.addEntry(stylesheet);
+            expect(generator.buffer).to.eql([]);
+        });
     });
 
 });
