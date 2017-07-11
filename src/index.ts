fa5c4a9b7a1a08eeeff9d4f5f821893d2f0bcbb1
Barak Igal <baraki@lan.wixpress.com>
2017-07-11 15:19:05 +0300

    export objectifyCSS

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Resolver } from "./resolver";
 export { Stylesheet, TypedClass } from "./stylesheet";
 export { Generator, DEFAULT_CONFIG, Config } from "./generator";
+export { objectifyCSS } from "./parser";
