432cdf69092293efc4e8c0849145f55513cc9bd6
Tal Harel <talh@wix.com>
2017-07-13 18:10:19 +0300

    Webpack config to bundle Stylable

--- src/stylable-component/stylable-react.ts
+++ src/stylable-component/stylable-react.ts
@@ -6,6 +6,10 @@ export { StylableContext, StyleableStylesheet };
 
 export const { SBComponent, SBStateless, Stylesheet, defineMixin } = createSBComponentFactory(createStyleableStylesheet());
 
+export { Resolver } from "../resolver";
+export { Generator, DEFAULT_CONFIG, Config } from "../generator";
+export { objectifyCSS } from "../parser";
+
 declare module 'react' {
     interface HTMLAttributes<T> extends SBComponentProps { }
 }
---
+++ webpack.pack.config.js
@@ -0,0 +1,28 @@
+var glob = require('glob');
+
+module.exports = {
+    devtool: 'eval',
+    entry: {
+        stylable: "./src/stylable-component/stylable-react.ts"
+    },
+    output: {
+        filename: '[name].bundle.js',
+        libraryTarget: 'umd',
+        library: 'stylable'
+    },
+    externals: [
+        'react'
+    ],
+    target: 'web',
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: ['.ts', '.tsx', '.js'] // note if using webpack 1 you'd also need a '' in the array as well
+    },
+    module: {
+        loaders: [ // loaders will work with webpack 1 or 2; but will be renamed "rules" in future
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            { test: /\.tsx?$/, loader: 'ts-loader' }
+        ]
+    }
+};
+
