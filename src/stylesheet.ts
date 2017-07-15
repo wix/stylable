7b242b74330d27b57a406f923439f9a13fee3a8a
Barak Igal <baraki@lan.wixpress.com>
2017-07-15 13:37:35 +0300

    refactor

--- src/stylesheet.ts
+++ src/stylesheet.ts
@@ -7,28 +7,21 @@ import { parseSelector, PseudoSelectorAstNode, createSimpleSelectorChecker, trav
 const mixMixin = SBTypesParsers[valueMapping.mixin];
 
 export class Stylesheet {
-    cssDefinition: CSSObject;
     namespace: string;
-    classes: Pojo<string>;
-    typedClasses: Pojo<TypedClass>;
-    mixinSelectors: Pojo<MixinValue[]>;
-    resolvedSymbols: Pojo<any>;
-    vars: Pojo<string>;
-    imports: Import[];
-    root: string;
+    root: string = 'root';
     source: string;
+    cssDefinition: CSSObject;
+    imports: Import[] = [];
+    classes: Pojo<string> = {};
+    vars: Pojo<string> = {};
+    mixinSelectors: Pojo<MixinValue[]> = {};
+    typedClasses: Pojo<TypedClass> = {};
     _kind = "Stylesheet";
     static globalCounter: number = 0;
     constructor(cssDefinition: CSSObject, namespace: string = "", source: string = "") {
         this.source = source;
         this.cssDefinition = cssDefinition;
-        this.root = 'root';
-        this.classes = {};
-        this.typedClasses = {};
-        this.vars = {};
-        this.mixinSelectors = {};
-        this.imports = [];
-        this.namespace = this.getNamespace(namespace, cssDefinition['@namespace']);
+        this.namespace = this.processNamespace(namespace, cssDefinition['@namespace']);
         this.process();
     }
     static fromCSS(css: string, namespace?: string, source?: string) {
@@ -42,7 +35,7 @@ export class Stylesheet {
         for (const selector in this.cssDefinition) {
             this.processDefinition(selector, this.cssDefinition[selector]);
         }
-        if(!this.typedClasses[this.root]){
+        if (!this.typedClasses[this.root]) {
             this.classes.root = this.root;
             this.typedClasses.root = { [valueMapping.root]: true };
         }
@@ -73,31 +66,7 @@ export class Stylesheet {
         });
         this.addTypedClasses(selector, rules, isSimpleSelector);
     }
-    private addTypedClasses(selector: string, rules: CSSRulesObject, isSimpleSelector: boolean) {
-        this.addTypedClass(selector, rules, isSimpleSelector, valueMapping.root);
-        this.addTypedClass(selector, rules, isSimpleSelector, valueMapping.states);
-        this.addTypedClass(selector, rules, isSimpleSelector, valueMapping.type);
-    }
-    private addTypedClass(selector: string, rules: CSSRulesObject, isSimpleSelector: boolean, rule: keyof typeof SBTypesParsers) {
-        if (hasOwn(rules, rule)) {
-            if (!isSimpleSelector) {
-                throw new Error(rule + ' on complex selector: ' + selector);
-            }
-            const name = selector.replace('.', '');
-            this.typedClasses[name] = {
-                ...this.typedClasses[name],
-                [rule]: SBTypesParsers[rule](rules[rule])
-            };
-        }
-    }
-    private addMixins(selector: string, rules: CSSRulesObject) {
-        let mixin: string | string[] = rules[valueMapping.mixin];
-        if (mixin && !Array.isArray(mixin)) { mixin = [mixin]; }
-        if (mixin) {
-            this.mixinSelectors[selector] = mixMixin(mixin[mixin.length - 1]);
-        }
-    }
-    private getNamespace(strongNamespace = "", weakNamespace: string | string[] = "") {
+    private processNamespace(strongNamespace = "", weakNamespace: string | string[] = "") {
         if (strongNamespace) { return strongNamespace.replace(/'|"/g, ''); }
         if (Array.isArray(weakNamespace)) {
             return weakNamespace[weakNamespace.length - 1].replace(/'|"/g, '');
@@ -105,6 +74,27 @@ export class Stylesheet {
             return weakNamespace.replace(/'|"/g, '');
         } else {
             return 's' + Stylesheet.globalCounter++;
+        }
+    }
+    private addTypedClasses(selector: string, rules: CSSRulesObject, isSimpleSelector: boolean) {
+        this.addTypedClass(selector, rules, isSimpleSelector, valueMapping.root);
+        this.addTypedClass(selector, rules, isSimpleSelector, valueMapping.states);
+        this.addTypedClass(selector, rules, isSimpleSelector, valueMapping.type);
+    }
+    private addTypedClass(selector: string, rules: CSSRulesObject, isSimpleSelector: boolean, typedRule: keyof typeof SBTypesParsers) {
+        if (!hasOwn(rules, typedRule)) { return; }
+        if (!isSimpleSelector) { throw new Error(typedRule + ' on complex selector: ' + selector); }
+        const name = selector.replace('.', '');
+        this.typedClasses[name] = {
+            ...this.typedClasses[name],
+            [typedRule]: SBTypesParsers[typedRule](rules[typedRule])
+        };
+    }
+    private addMixins(selector: string, rules: CSSRulesObject) {
+        let mixin: string | string[] = rules[valueMapping.mixin];
+        if (mixin && !Array.isArray(mixin)) { mixin = [mixin]; }
+        if (mixin) {
+            this.mixinSelectors[selector] = mixMixin(mixin[mixin.length - 1]);
         }
     }
     /********************************************/
