a20652642c97219d84ce3275aeb3829b5f6c5728
Barak Igal <baraki@lan.wixpress.com>
2017-07-16 12:43:57 +0300

    add support for loader

--- src/stylable-componentv2/stylable-react.ts
+++ src/stylable-componentv2/stylable-react.ts
@@ -5,29 +5,31 @@ import { Stylesheet } from "../stylesheet";
 
 import { StylableComponent, SBStatelessComponent, StateLess, SBComponentProps } from "./types";
 
+export type StylesheetInput = Stylesheet | {$stylesheet: Stylesheet};
+
 export const context = new StylableContext({ namespaceDivider: "▪" });
 
-function SBComponent<T extends React.ComponentClass<any>>(Base: T, stylesheet: Stylesheet): StylableComponent<T>;
-function SBComponent<T>(stylesheet: Stylesheet): Function;
-function SBComponent<T extends React.ComponentClass<any>>(Base: T | Stylesheet, stylesheet?: Stylesheet) {
+function SBComponent<T extends React.ComponentClass<any>>(Base: T, stylesheet: StylesheetInput): StylableComponent<T>;
+function SBComponent<T>(stylesheet: StylesheetInput): Function;
+function SBComponent<T extends React.ComponentClass<any>>(Base: T | StylesheetInput, stylesheet?: StylesheetInput) {
     if (!stylesheet) {
         return function (Component: T): StylableComponent<T> {
             return SBComponent(Component, Base as Stylesheet);
         }
     }
+    const sheet =  (<{$stylesheet: Stylesheet}>stylesheet).$stylesheet || stylesheet;
     const Class = Base as any;
-    Class.prototype.render = wrapSBRender(Class.prototype.render, stylesheet);
-    Class.toString = function () { return stylesheet.namespace; }
-    context.add(stylesheet);
+    Class.prototype.render = wrapSBRender(Class.prototype.render, sheet);
+    Class.toString = function () { return sheet.namespace; }
+    context.add(sheet); //WAT
     return Class;
 }
 
-function SBStateless<T, C = object>(renderFunction: StateLess<T, C>, stylesheet: Stylesheet): SBStatelessComponent<T> {
-    context.add(stylesheet);
-    const wrapped = wrapSBRender(renderFunction as StateLess<T, C>, stylesheet);
-    wrapped.toString = function () {
-        return stylesheet.namespace;
-    }
+function SBStateless<T, C = object>(renderFunction: StateLess<T, C>, stylesheet: StylesheetInput): SBStatelessComponent<T> {
+    const sheet =  (<{$stylesheet: Stylesheet}>stylesheet).$stylesheet || stylesheet;
+    const wrapped = wrapSBRender(renderFunction as StateLess<T, C>, sheet);
+    wrapped.toString = function () { return sheet.namespace; }
+    context.add(sheet); //WAT
     return wrapped;
 }
 
