7225a41d1f342edbd4e0a229f16748c5ac0aa5c1
Barak Igal <baraki@lan.wixpress.com>
2017-07-19 17:23:09 +0300

    move stylable react to seperate project

--- src/stylable-component/stylable-react.ts
+++
@@ -1,7 +0,0 @@
-import { Stylesheet as StyleableStylesheet } from '../index';
-import { createSBComponentFactory } from './component';
-import { createStyleableStylesheet, StylableContext } from "./create-styleable-stylesheet";
-
-export { StylableContext, StyleableStylesheet };
-
-export const { SBComponent, SBStateless, Stylesheet, defineMixin } = createSBComponentFactory(createStyleableStylesheet());
