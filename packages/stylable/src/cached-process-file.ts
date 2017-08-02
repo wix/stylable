968f0eb07881de6b57040f21ee68ac7a6d5ec12a
Barak Igal <baraki@lan.wixpress.com>
2017-08-02 05:12:06 +0300

    change cacheProcessFile

--- packages/stylable/src/cached-process-file.ts
+++ packages/stylable/src/cached-process-file.ts
@@ -1,7 +1,7 @@
 export type processFn<T> = (fullpath: string, content: string) => T;
 
 export interface CacheItem<T> {
-    content: string, value: T, stat: { mtime: Date }
+    value: T, stat: { mtime: Date }
 }
 
 export interface MinimalFS {
@@ -9,16 +9,35 @@ export interface MinimalFS {
     readFileSync: (fullpath: string, encoding: string) => string
 }
 
-export function cachedProcessFile<T = any>(process: processFn<T>, fs: MinimalFS): (fullpath: string) => T {
-    var cache: { [key: string]: CacheItem<T> } = {};
-    return function (fullpath: string) {
+export interface FileProcessor<T> {
+    process: (fullpath: string) => T;
+    add: (fullpath: string, value: T) => void;
+}
+
+export function cachedProcessFile<T = any>(processor: processFn<T>, fs: MinimalFS): FileProcessor<T> {
+    const cache: { [key: string]: CacheItem<T> } = {};
+
+    const process = function (fullpath: string) {
         var stat = fs.statSync(fullpath);
         var cached = cache[fullpath];
         if (!cached || (cached && cached.stat.mtime.valueOf() !== stat.mtime.valueOf())) {
             var content = fs.readFileSync(fullpath, 'utf8');
-            var value = process(fullpath, content);
-            cache[fullpath] = { content, value, stat };
+            var value = processor(fullpath, content);
+            cache[fullpath] = { value, stat };
         }
         return cache[fullpath].value;
     }
+
+    const add = function (fullpath: string, value: T) {
+        cache[fullpath] = {
+            value,
+            stat: fs.statSync(fullpath)
+        }
+    }
+
+    return {
+        process,
+        add
+    };
+
 }
