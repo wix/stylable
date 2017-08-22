4f0aafa0555816c3d0db3965023b6e50cbd49951
Barak Igal <baraki@lan.wixpress.com>
2017-08-22 17:03:30 +0300

    chage fileProcessor to be protected

--- src/postcss-resolver.ts
+++ src/postcss-resolver.ts
@@ -17,7 +17,7 @@ export interface JSResolve {
 }
 
 export class StylableResolver {
-    constructor(private fileProcessor: FileProcessor<StylableMeta>, private requireModule: (modulePath: string) => any) {}
+    constructor(protected fileProcessor: FileProcessor<StylableMeta>, protected requireModule: (modulePath: string) => any) {}
     resolveVarValue(meta: StylableMeta, name: string) {
         let value;
         let symbol = meta.mappedSymbols[name];
