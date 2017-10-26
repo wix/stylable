0d3aa6227d4fafebbd192981bde7b2db0334dd23
Avi Vahl <avi.vahl@wix.com>
2017-10-26 17:50:09 +0300

    Stylable no longer exports a global namespace duplicate types for now

--- packages/stylable-scripts/template/src/globals.d.ts
+++ packages/stylable-scripts/template/src/globals.d.ts
@@ -1,11 +1,22 @@
-/// <reference path="../node_modules/stylable/runtime.d.ts" />
+interface StateMap {
+    [key: string]: boolean
+}
+
+interface Stylesheet {
+    namespace: string;
+    root: string;
+    get: (localName: string) => string;
+    cssStates: (stateMapping: StateMap) => StateMap;
+}
+
+type RuntimeStylesheet = {[key: string]: string} & {$stylesheet: Stylesheet};
 
 declare module '*.svg' {
     const urlToFile: string;
     export default urlToFile;
 }
 declare module '*.st.css' {
-    const stylesheet: Stylable.RuntimeStylesheet;
+    const stylesheet: RuntimeStylesheet;
     export default stylesheet;
 }
 
