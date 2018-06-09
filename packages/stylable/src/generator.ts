ec8670cf2493f6d766c8333fb7d74aff685ab873
Barak Igal <baraki@wix.com>
2018-06-09 22:41:08 +0300

    Stylable: refactor generator to work with new file processor

--- packages/stylable/src/generator.ts
+++ packages/stylable/src/generator.ts
@@ -1,10 +1,10 @@
-import {cachedProcessFile, FileProcessor, MinimalFS} from './cached-process-file';
-import {Diagnostics} from './diagnostics';
-import {safeParse} from './parser';
-import {create} from './runtime';
-import {process, StylableMeta} from './stylable-processor';
-import {StylableTransformer} from './stylable-transformer';
-import {RuntimeStylesheet} from './types';
+import { cachedProcessFile, FileProcessor, MinimalFS } from './cached-process-file';
+import { Diagnostics } from './diagnostics';
+import { safeParse } from './parser';
+import { create } from './runtime';
+import { process, StylableMeta } from './stylable-processor';
+import { StylableTransformer } from './stylable-transformer';
+import { RuntimeStylesheet } from './types';
 
 export function createGenerator(
     fs: MinimalFS = {
@@ -12,15 +12,19 @@ export function createGenerator(
             return '';
         },
         statSync(_path: string) {
-            return {mtime: new Date(0)};
+            return { mtime: new Date(0) };
         }
     },
     requireModule: (moduleId: string) => any = (_path: string): any => ({}),
-    delimiter: string = '--') {
-
-    const fileProcessor: FileProcessor<StylableMeta> = cachedProcessFile<StylableMeta>((from, content) => {
-        return process(safeParse(content, {from}));
-    }, fs);
+    delimiter: string = '--'
+) {
+    const fileProcessor: FileProcessor<StylableMeta> = cachedProcessFile<StylableMeta>(
+        (from, content) => {
+            return process(safeParse(content, { from }));
+        },
+        fs,
+        x => x
+    );
 
     function output(meta: StylableMeta) {
         const diagnostics = new Diagnostics();
@@ -32,13 +36,19 @@ export function createGenerator(
             delimiter
         });
 
-        const {exports} = transformer.transform(meta);
+        const { exports } = transformer.transform(meta);
 
         return {
             meta,
             transformer,
             diagnostics,
-            runtime: create(meta.root, meta.namespace, exports, '', meta.source) as RuntimeStylesheet
+            runtime: create(
+                meta.root,
+                meta.namespace,
+                exports,
+                '',
+                meta.source
+            ) as RuntimeStylesheet
         };
     }
 
@@ -47,7 +57,7 @@ export function createGenerator(
         delimiter,
         scope: StylableTransformer.prototype.scope,
         fromCSS(source: string, path: string = '/unknown.st.css') {
-            const root = safeParse(source, {from: path});
+            const root = safeParse(source, { from: path });
             const meta = process(root);
             fileProcessor.add(meta.source, meta);
             return output(meta);
@@ -57,5 +67,4 @@ export function createGenerator(
             return output(meta);
         }
     };
-
 }
