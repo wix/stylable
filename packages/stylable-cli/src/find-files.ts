f01f0cdf83f227da80a4da9a02e7cbb1119710c0
Barak Igal <baraki@wix.com>
2018-06-09 23:11:22 +0300

    * Added stylable-cli project

---
+++ packages/stylable-cli/src/find-files.ts
@@ -0,0 +1,33 @@
+import { join } from 'path';
+
+export type FileSystem = any;
+
+export function findFiles(fs: FileSystem, rootDirectory: string, ext: string, blacklist: Set<string>) {
+    const errors: Error[] = [];
+    const result: string[] = [];
+    const folders = [rootDirectory];
+    while (folders.length) {
+        const current = folders.pop()!;
+        try {
+            fs.readdirSync(current).forEach((item: string) => {
+                if (blacklist.has(item)) {
+                    return;
+                }
+                const itemFullPath = join(current, item);
+                try {
+                    const status = fs.statSync(itemFullPath);
+                    if (status.isDirectory()) {
+                        folders.push(itemFullPath);
+                    } else if (status.isFile() && itemFullPath.endsWith(ext)) {
+                        result.push(itemFullPath);
+                    }
+                } catch (e) {
+                    errors.push(e);
+                }
+            });
+        } catch (e) {
+            errors.push(e);
+        }
+    }
+    return { result, errors };
+}
