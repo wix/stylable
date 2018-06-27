6104f3c714fa70786e1a786df4c8d2d33a600a03
Barak Igal <baraki@wix.com>
2018-06-27 23:54:43 +0300

    move all webpack plugin tests to ts

--- packages/stylable-webpack-plugin/test/e2e/4th-party-project.spec.ts
+++ packages/stylable-webpack-plugin/test/e2e/4th-party-project.spec.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import { join } from 'path';
 import { browserFunctions, StylableProjectRunner } from 'stylable-build-test-kit';
 
-const project = 'dynamic-chunk-depth-project';
+const project = '4th-party-project';
 
 describe(`(${project})`, () => {
     const projectRunner = StylableProjectRunner.mochaSetup(
@@ -10,7 +10,8 @@ describe(`(${project})`, () => {
             projectDir: join(__dirname, 'projects', project),
             port: 3001,
             puppeteerOptions: {
-                // headless: false
+                // headless: false,
+                // devtools: true
             }
         },
         before,
@@ -20,11 +21,6 @@ describe(`(${project})`, () => {
 
     it('renders css', async () => {
         const { page } = await projectRunner.openInBrowser();
-        const styleElements = await page.evaluate(browserFunctions.getStyleElementsMetadata);
-
-        expect(styleElements).to.eql([
-            { id: './src/button.st.css', depth: '1' },
-            { id: './src/gallery.st.css', depth: '2' }
-        ]);
+        // TODO: add expect
     });
 });
---
+++ packages/stylable-webpack-plugin/test/e2e/bad-project.spec.ts
@@ -0,0 +1,30 @@
+import { expect } from 'chai';
+import { join } from 'path';
+import { StylableProjectRunner } from 'stylable-build-test-kit';
+
+const project = 'bad-project';
+
+describe(`(${project})`, () => {
+    const projectRunner = StylableProjectRunner.mochaSetup(
+        {
+            projectDir: join(__dirname, 'projects', project),
+            port: 3001,
+            throwOnBuildError: false,
+            puppeteerOptions: {
+                // headless: false
+            }
+        },
+        before,
+        afterEach,
+        after
+    );
+
+    it('reports warnings', async () => {
+        const warnings = projectRunner.getBuildWarningMessages();
+        const expected = [/Could not resolve 'unknown'/, /unknown var "xxx"/];
+        expect(warnings.length).to.equal(2);
+        warnings.forEach((warning: string, i: number) => {
+            expect(warning).to.match(expected[i]);
+        });
+    });
+});
