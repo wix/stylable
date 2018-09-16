0c6aff2a63e2d8a7f2fafe9bdd8bbdf1b2eb10cd
Avi Vahl <avi.vahl@wix.com>
2018-09-16 15:15:21 +0300

    fix typo

--- packages/jest/src/jest.ts
+++ packages/jest/src/jest.ts
@@ -9,5 +9,5 @@ export const process = stylableModuleFactory(
     },
     // ensure the generated module points to our own @stylable/runtime copy
     // this allows @stylable/jest to be used as part of a globally installed CLI
-    require.resolve('@stylable/runtime/package.json')
+    require.resolve('@stylable/runtime')
 );
