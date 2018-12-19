0c6bc9927e073c09e8591faa6630ef6d6721b2f4
Tom Raviv <tomr@wix.com>
2018-12-19 16:10:37 +0200

    Add option to mute "duplicate module namespace" in webpack config (#477)
    * add option to mute "duplicate module namespace" in webpack config
    * add test to check new error muting functionality

---
+++ packages/webpack-plugin/test/e2e/projects/unsafe-mute-diagnostics/webpack.config.js
@@ -0,0 +1,13 @@
+const StylableWebpackPlugin = require("../../../../src");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  context: __dirname,
+  devtool: "source-map",
+  plugins: [new StylableWebpackPlugin({
+    unsafeMuteDiagnostics: {
+      DUPLICATE_MODULE_NAMESPACE: true
+    }
+  }), new HtmlWebpackPlugin()]
+};
