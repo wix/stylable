cd77e6db3379400b5093c8431870907bf7438ec7
Barak Igal <baraki@wix.com>
2019-01-16 13:57:39 +0200

    update configs

--- packages/webpack-plugin/test/e2e/projects/dynamic-chunk-with-stylable-production/webpack.config.js
+++ packages/webpack-plugin/test/e2e/projects/dynamic-chunk-with-stylable-production/webpack.config.js
@@ -1,13 +1,14 @@
-const StylableWebpackPlugin = require("../../../../src");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const StylableWebpackPlugin = require('../../../../src');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "production",
-  context: __dirname,
-  devtool: "source-map",
-  plugins: [new StylableWebpackPlugin({
-    useEntryModuleInjection: true,
-    useWeakDeps: true,
-    includeDynamicModulesInCSS: true
-  }), new HtmlWebpackPlugin()]
+    mode: 'production',
+    context: __dirname,
+    devtool: 'source-map',
+    plugins: [
+        new StylableWebpackPlugin({
+            useEntryModuleInjection: true
+        }),
+        new HtmlWebpackPlugin()
+    ]
 };
--- packages/webpack-plugin/test/e2e/projects/dynamic-chunk-with-stylable/webpack.config.js
+++ packages/webpack-plugin/test/e2e/projects/dynamic-chunk-with-stylable/webpack.config.js
@@ -1,12 +1,14 @@
-const StylableWebpackPlugin = require("../../../../src");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const StylableWebpackPlugin = require('../../../../src');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  context: __dirname,
-  devtool: "source-map",
-  plugins: [new StylableWebpackPlugin({
-    useEntryModuleInjection: true,
-    useWeakDeps: true
-  }), new HtmlWebpackPlugin()]
+    mode: 'development',
+    context: __dirname,
+    devtool: 'source-map',
+    plugins: [
+        new StylableWebpackPlugin({
+            useEntryModuleInjection: true
+        }),
+        new HtmlWebpackPlugin()
+    ]
 };
