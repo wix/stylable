3e4444c1991eb8db69ef5d46a08a14775d48184a
tomr <tomr@wix.com>
2019-02-19 14:33:31 +0200

    webpack-extensions: refactor test to the new runtime API

--- packages/webpack-extensions/test/projects/html-snapshot-plugin-project/src/index.js
+++ packages/webpack-extensions/test/projects/html-snapshot-plugin-project/src/index.js
@@ -2,5 +2,5 @@ import { createElement as el } from 'react';
 import style from './index.st.css';
 
 export function Index() {
-    return el('div', style('root'), 'Hello World');
+    return el('div', { className: style.root }, 'Hello World');
 }
