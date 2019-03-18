209f31d66d0349e7f2619aa8cd0646aa7bfe1312
tomr <tomr@wix.com>
2019-03-18 16:37:24 +0200

    node: expose namespace resolution factory method

--- packages/node/src/resolve-namespace.ts
+++ packages/node/src/resolve-namespace.ts
@@ -3,9 +3,13 @@ import hash from 'murmurhash';
 import { dirname, relative } from 'path';
 const findConfig = require('find-config');
 
-export const resolveNamespace: typeof processNamespace = (namespace: string, stylesheetPath: string) => {
-    const configPath = findConfig('package.json', { cwd: dirname(stylesheetPath) });
-    const config = require(configPath);
-    const fromRoot = relative(dirname(configPath), stylesheetPath).replace(/\\/g, '/');
-    return namespace + hash.v3(config.name + '@' + config.version + '/' + fromRoot);
-};
+export function resolveNamespaceFactory(prefix: string): typeof processNamespace {
+    return (namespace: string, stylesheetPath: string) => {
+        const configPath = findConfig('package.json', { cwd: dirname(stylesheetPath) });
+        const config = require(configPath);
+        const fromRoot = relative(dirname(configPath), stylesheetPath).replace(/\\/g, '/');
+        return prefix + namespace + hash.v3(config.name + '@' + config.version + '/' + fromRoot);
+    };
+}
+
+export const resolveNamespace = resolveNamespaceFactory('');
