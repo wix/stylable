b065594821c83bc3721268d86e1bda7a6a3c04ea
barak igal <baraki@wix.com>
2019-03-20 14:21:31 +0200

    update resolveNamespaceFactory
    allow hashSalt and prefix

--- packages/node/src/resolve-namespace.ts
+++ packages/node/src/resolve-namespace.ts
@@ -3,13 +3,13 @@ import hash from 'murmurhash';
 import { dirname, relative } from 'path';
 const findConfig = require('find-config');
 
-export function resolveNamespaceFactory(prefix: string): typeof processNamespace {
+export function resolveNamespaceFactory(hashSalt: string = '', prefix: string = ''): typeof processNamespace {
     return (namespace: string, stylesheetPath: string) => {
         const configPath = findConfig('package.json', { cwd: dirname(stylesheetPath) });
         const config = require(configPath);
         const fromRoot = relative(dirname(configPath), stylesheetPath).replace(/\\/g, '/');
-        return prefix + namespace + hash.v3(config.name + '@' + config.version + '/' + fromRoot);
+        return prefix + namespace + hash.v3(hashSalt + config.name + '@' + config.version + '/' + fromRoot);
     };
 }
 
-export const resolveNamespace = resolveNamespaceFactory('');
+export const resolveNamespace = resolveNamespaceFactory();
