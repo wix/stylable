707083b319869229d963c187b6af1ea418ac2dd2
barak igal <baraki@wix.com>
2019-06-05 11:31:19 +0300

    fix webpack plugins api (#649)

--- packages/webpack-plugin/src/types.ts
+++ packages/webpack-plugin/src/types.ts
@@ -15,7 +15,7 @@ export interface StylableWebpackPluginOptions {
     runtimeMode: 'isolated' | 'shared' | 'external';
     globalRuntimeId: string;
     bootstrap: {
-        autoInit: boolean,
+        autoInit: boolean;
         getAutoInitModule?: any;
         globalInjection?: (p: string) => string;
     };
@@ -37,8 +37,10 @@ export interface StylableWebpackPluginOptions {
     unsafeMuteDiagnostics: {
         DUPLICATE_MODULE_NAMESPACE: boolean;
     };
-    afterTransform?: ((results: StylableResults, module: StylableModule, stylable: Stylable) => void) | null;
-    plugins?: Array<(this: webpack.Compiler, ...p: any[]) => void>;
+    afterTransform?:
+        | ((results: StylableResults, module: StylableModule, stylable: Stylable) => void)
+        | null;
+    plugins?: Array<{ apply: (compiler: webpack.Compiler, stylablePlugin: any) => void }>;
     resolveNamespace?(): string;
     requireModule(path: string): any;
 }
@@ -50,7 +52,9 @@ export interface StylableGeneratorOptions {
     afterTransform: any;
 }
 
-export type ShallowPartial<T> = {[P in keyof T]?: T[P] extends new() => any ? T[P] : Partial<T[P]>};
+export type ShallowPartial<T> = {
+    [P in keyof T]?: T[P] extends new () => any ? T[P] : Partial<T[P]>
+};
 
 export interface CalcResult {
     depth: number;
@@ -69,12 +73,12 @@ export interface StylableModule extends webpack.Module {
     request: string;
     loaders: webpack.NewLoader[];
     buildInfo: {
-        optimize: StylableWebpackPluginOptions['optimize']
+        optimize: StylableWebpackPluginOptions['optimize'];
         isImportedByNonStylable: boolean;
         runtimeInfo: CalcResult;
         stylableMeta: StylableMeta;
-        usageMapping: Record<string, boolean>
-        usedStylableModules: StylableModule[]
+        usageMapping: Record<string, boolean>;
+        usedStylableModules: StylableModule[];
     };
     originalSource(): string;
 }
