fa6c923981794b52d3c0e8aa34aaca6f18cc7007
Barak Igal <baraki@wix.com>
2019-08-01 12:53:53 +0300

    add unsafeBuildNamespace option

---
+++ packages/webpack-plugin/test/e2e/3rd-party-standalone.spec.ts
@@ -0,0 +1,41 @@
+import { browserFunctions, StylableProjectRunner } from '@stylable/e2e-test-kit';
+import { expect } from 'chai';
+import { join } from 'path';
+
+const project = '3rd-party-standalone';
+
+describe(`(${project})`, () => {
+    const projectRunner = StylableProjectRunner.mochaSetup(
+        {
+            projectDir: join(__dirname, 'projects', project),
+            puppeteerOptions: {
+                // headless: false
+            }
+        },
+        before,
+        afterEach,
+        after
+    );
+
+    it('renders css', async () => {
+        const { page } = await projectRunner.openInBrowser();
+        const styleElements = await page.evaluate(browserFunctions.getStyleElementsMetadata);
+
+        expect(styleElements).to.eql([
+            { id: './node_modules/test-components/button.st.css', depth: '1' },
+            // {
+            //     id: './node_modules/test-components/index.st.css',
+            //     depth: '2'
+            // },
+            { id: './src/index.st.css', depth: '3' }
+        ]);
+    });
+
+    it('override 3rd party', async () => {
+        const { page } = await projectRunner.openInBrowser();
+        const backgroundColor = await page.evaluate(() => {
+            return getComputedStyle((window as any).btn).backgroundColor;
+        });
+        expect(backgroundColor).to.eql('rgb(0, 128, 0)');
+    });
+});
