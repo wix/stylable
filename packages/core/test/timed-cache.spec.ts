e2ea5489613a4e4e6757d4420bdb3cda1af5dc8d
Barak Igal <baraki@wix.com>
2019-11-13 14:42:57 +0200

    format

--- packages/core/test/timed-cache.spec.ts
+++ packages/core/test/timed-cache.spec.ts
@@ -5,7 +5,6 @@ const delay = (time: number) => new Promise(res => setTimeout(res, time));
 import { timedCache } from '../src/timed-cache';
 
 describe.only('timed-cache', () => {
-
     it('should cache for specific time', async () => {
         const spy = Sinon.spy((...args: string[]) => args.join(';'));
         const cached = timedCache(spy, {
@@ -17,23 +16,21 @@ describe.only('timed-cache', () => {
         cached.get('1');
         cached.get('2');
         cached.get('3');
-        
+
         cached.get('1');
         cached.get('2');
         cached.get('3');
-        
+
         cached.get('1');
         cached.get('2');
         cached.get('3');
 
         expect(spy.callCount, 'calls').to.equal(3);
         expect(cached.cache.size, 'cache size').to.equal(3);
-        await delay(101)
+        await delay(101);
 
-        
         cached.get('1');
         expect(cached.cache.size, 'cache size after clean up').to.equal(1);
-        
     });
 
     it('should cache for specific time with timer auto clean', async () => {
@@ -47,21 +44,19 @@ describe.only('timed-cache', () => {
         cached.get('1');
         cached.get('2');
         cached.get('3');
-        
+
         cached.get('1');
         cached.get('2');
         cached.get('3');
-        
+
         cached.get('1');
         cached.get('2');
         cached.get('3');
 
         expect(spy.callCount, 'calls').to.equal(3);
         expect(cached.cache.size, 'cache size').to.equal(3);
-        await delay(101)
+        await delay(101);
 
         expect(cached.cache.size, 'cache size after clean up').to.equal(0);
-        
     });
-
 });
