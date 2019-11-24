12a1d9bda8fb007526215a716fbcd9d9ce391c7d
Avi Vahl <avi.vahl@wix.com>
2019-11-24 14:45:50 +0200

    jest: port #887 to 1.x series

--- packages/jest/src/jest.ts
+++ packages/jest/src/jest.ts
@@ -1,6 +1,8 @@
 import { stylableModuleFactory } from '@stylable/node';
 import * as fs from 'fs';
 
+const stylableRuntimePath = require.resolve('@stylable/runtime');
+
 export const process = stylableModuleFactory(
     {
         fileSystem: fs,
@@ -9,5 +11,16 @@ export const process = stylableModuleFactory(
     },
     // ensure the generated module points to our own @stylable/runtime copy
     // this allows @stylable/jest to be used as part of a globally installed CLI
-    require.resolve('@stylable/runtime')
+    stylableRuntimePath
 );
+
+export function getCacheKey(
+    fileData: string,
+    filename: string,
+    configString: string,
+    { instrument }: { instrument: boolean }
+) {
+    return (
+        fileData + configString + (instrument ? 'instrument' : '') + filename + stylableRuntimePath
+    );
+}
