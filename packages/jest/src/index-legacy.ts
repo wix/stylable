ef477badbcc475ce8a4c6197768d7347543a6f3a
Avi Vahl <avi.vahl@wix.com>
2020-05-13 19:02:50 +0300

    Two missed places

--- packages/jest/src/index-legacy.ts
+++
@@ -1,8 +0,0 @@
-import { processFactory } from './jest';
-
-export const process = processFactory(
-    {},
-    {
-        runtimePath: require.resolve('@stylable/runtime/cjs/index-legacy'),
-    }
-);
