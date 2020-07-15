854132ca4afcdfc83eae5d9774e8e8f556a01163
barak igal <baraki@wix.com>
2020-07-15 11:46:49 +0300

    Update webpack.config.ts

--- packages/experimental-loader/test/projects/errors-integration/webpack.config.ts
+++ packages/experimental-loader/test/projects/errors-integration/webpack.config.ts
@@ -1,37 +1,17 @@
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import HTMLWebpackPlugin from 'html-webpack-plugin';
 import { stylableLoaders } from '../../../src';
-import { noCollisionNamespace } from '@stylable/core';
 
 module.exports = {
     mode: 'development',
     entry: './index.js',
     context: __dirname,
     devtool: false,
-    plugins: [new MiniCssExtractPlugin(), new HTMLWebpackPlugin()],
     module: {
         rules: [
             {
-                test: /\.(png|jpg|gif)$/i,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 8192,
-                        },
-                    },
-                ],
-            },
-            {
                 test: /\.st\.css$/i,
                 use: [
-                    stylableLoaders.runtime(),
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: { esModule: true, reloadAll: true },
-                    },
                     stylableLoaders.transform({
-                        resolveNamespace: noCollisionNamespace(),
+                        exportsOnly: true,
                     }),
                 ],
             },
