66d41b829aee56d17dc18b73f66c57717aa0f72d
Tom Raviv <tomr@wix.com>
2020-07-26 19:36:04 +0300

    Set "@stylable/runtime" build target to ES5 (#1283)
    - use es5 compliant code in experimental-loader target code

--- packages/experimental-loader/src/create-runtime-target-code.ts
+++ packages/experimental-loader/src/create-runtime-target-code.ts
@@ -2,7 +2,7 @@ import { StylableExports } from '@stylable/core';
 
 export function createRuntimeTargetCode(namespace: string, mapping: StylableExports) {
     return `
-  const rt = require("@stylable/runtime/cjs/css-runtime-stylesheet.js");
+  var rt = require("@stylable/runtime/cjs/css-runtime-stylesheet.js");
 
   module.exports = rt.create(
       ${JSON.stringify(namespace)},
