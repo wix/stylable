9d31c8e9cfda9c89212b01d860c5bd995a5edd2f
Barak Igal <baraki@wix.com>
2020-08-09 12:34:58 +0300

    support for css loader v4

--- packages/experimental-loader/src/warning.ts
+++ packages/experimental-loader/src/warning.ts
@@ -1,5 +1,4 @@
 // Adopted from css-loader;
-
 export class Warning extends Error {
     constructor(warning: { text: string; line: number; column: number }) {
         super(String(warning));
@@ -15,6 +14,42 @@ export class Warning extends Error {
 
         this.message += text; // We don't need stack https://github.com/postcss/postcss/blob/master/docs/guidelines/runner.md#31-dont-show-js-stack-for-csssyntaxerror
 
+        this.stack = undefined;
+    }
+}
+
+interface PostCSSError {
+    reason: string;
+    line: string;
+    column: string;
+    showSourceCode(): string;
+}
+
+export class CssSyntaxError extends Error {
+    constructor(error: PostCSSError) {
+        super(error.toString());
+
+        const { reason, line, column } = error;
+
+        this.name = 'CssSyntaxError';
+
+        // Based on https://github.com/postcss/postcss/blob/master/lib/css-syntax-error.es6#L132
+        // We don't need `plugin` and `file` properties.
+        this.message = `${this.name}\n\n`;
+
+        if (typeof line !== 'undefined') {
+            this.message += `(${line}:${column}) `;
+        }
+
+        this.message += `${reason}`;
+
+        const code = error.showSourceCode();
+
+        if (code) {
+            this.message += `\n\n${code}\n`;
+        }
+
+        // We don't need stack https://github.com/postcss/postcss/blob/master/docs/guidelines/runner.md#31-dont-show-js-stack-for-csssyntaxerror
         this.stack = undefined;
     }
 }
