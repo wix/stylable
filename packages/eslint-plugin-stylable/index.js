93091b4f87f7a533f301ca56b8c72384120fd941
Barak Igal <baraki@wix.com>
2020-08-16 13:36:41 +0300

    add dev lint support

--- packages/eslint-plugin-stylable/index.js
+++ packages/eslint-plugin-stylable/index.js
@@ -1,1 +1,10 @@
-module.exports = require('./cjs/index').default
+const { normalize } = require('path');
+
+// Use ts-tools to run typescript from source when running in the context of this mono-repo
+if (__filename.endsWith(normalize('/packages/eslint-plugin-stylable/index.js'))) {
+    require('@ts-tools/node/r');
+    require('tsconfig-paths/register');
+    module.exports = require('./src/index').default;
+} else {
+    module.exports = require('./cjs/index').default;
+}
