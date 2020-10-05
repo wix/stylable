31d03824deb9294dee63aed4839d2ce9482fb2fa
Avi Vahl <avi.vahl@wix.com>
2020-10-05 22:40:12 +0300

    language-service: remove another unused interface

--- packages/language-service/src/lib/types.ts
+++ packages/language-service/src/lib/types.ts
@@ -1,26 +1,4 @@
 import ts from 'typescript';
-import {
-    Command,
-    CompletionItem,
-    Diagnostic,
-    Location,
-    ParameterInformation,
-    Position,
-    Range,
-    TextEdit,
-} from 'vscode-css-languageservice';
-
-export interface LSPTypeHelpers {
-    // TODO: remove me?
-    CompletionItem: typeof CompletionItem;
-    TextEdit: typeof TextEdit;
-    Location: typeof Location;
-    Range: typeof Range;
-    Position: typeof Position;
-    Command: typeof Command;
-    ParameterInformation: typeof ParameterInformation;
-    Diagnostic: typeof Diagnostic;
-}
 
 export interface ExtendedTsLanguageService {
     setOpenedFiles: (files: string[]) => void;
