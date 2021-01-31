17022919769e626575be621f9ff4b52f1e6924b0
barak igal <baraki@wix.com>
2021-01-31 15:27:30 +0200

    fix(core): use correct context when resolving js mixins (#1607)

---
+++ packages/webpack-plugin/test/e2e/resolve-js-with-context.spec.ts
@@ -0,0 +1,28 @@
+import { StylableProjectRunner } from '@stylable/e2e-test-kit';
+import { expect } from 'chai';
+import { join } from 'path';
+
+const project = 'resolve-js-with-context';
+
+describe(`(${project})`, () => {
+    const projectRunner = StylableProjectRunner.mochaSetup(
+        {
+            projectDir: join(__dirname, 'projects', project),
+            puppeteerOptions: {
+                // headless: false
+            },
+        },
+        before,
+        afterEach,
+        after
+    );
+
+    it('css is working', async () => {
+        const { page } = await projectRunner.openInBrowser();
+        const color = await page.evaluate(() => {
+            return getComputedStyle(document.documentElement).color;
+        });
+
+        expect(color).to.eql('rgb(255, 0, 0)');
+    });
+});
