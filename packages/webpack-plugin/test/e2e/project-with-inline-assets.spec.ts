73841f3270565b35678fc287bd381ddacb4677c2
Barak Igal <baraki@wix.com>
2021-02-09 14:19:23 +0200

    remvoe unused imports

--- packages/webpack-plugin/test/e2e/project-with-inline-assets.spec.ts
+++ packages/webpack-plugin/test/e2e/project-with-inline-assets.spec.ts
@@ -1,19 +1,8 @@
-import {
-    browserFunctions,
-    filterAssetResponses,
-    StylableProjectRunner,
-} from '@stylable/e2e-test-kit';
+import { browserFunctions, StylableProjectRunner } from '@stylable/e2e-test-kit';
 import { expect } from 'chai';
 import { join } from 'path';
 
 const project = 'project-with-inline-assets';
-
-const expectedAssets = [
-    /* 1px-cyan.png */
-    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAMSURBVBhXY2BY9AIAAjABi8G3mj0AAAAASUVORK5CYII=',
-    /* 1px-red.png */
-    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAMSURBVBhXY3growIAAycBLhVrvukAAAAASUVORK5CYII=',
-];
 
 describe(`(${project})`, () => {
     const projectRunner = StylableProjectRunner.mochaSetup(
