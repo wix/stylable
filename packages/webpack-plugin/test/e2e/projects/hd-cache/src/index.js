bfead010e6bc7501b8e1ee6dc8c01707135b8dd2
Barak Igal <baraki@wix.com>
2021-03-18 15:12:14 +0200

    fix possible hd-cache webpack issue

---
+++ packages/webpack-plugin/test/e2e/projects/hd-cache/src/index.js
@@ -0,0 +1,5 @@
+import { button } from 'test-components';
+const btn = button.render('I am a button!!!!!');
+btn.id = 'btn';
+document.body.appendChild(btn);
+// document.body.appendChild(box.render());
