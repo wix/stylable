267d8116ebdd5af03b90635a7dbc68395ca240da
Barak Igal <baraki@wix.com>
2021-04-22 14:43:34 +0300

    removed unnecessary st call

--- packages/rollup-plugin/test/projects/simple-stylable/index.js
+++ packages/rollup-plugin/test/projects/simple-stylable/index.js
@@ -1,4 +1,7 @@
 import { st, cssStates, classes } from './index.st.css';
 
-document.body.classList.add(st(classes.root));
-document.body.innerHTML = `Hello<div class="${st(classes.part, cssStates({myState: true}))}">Stylable</div>`;
+document.body.classList.add(classes.root);
+document.body.innerHTML = `Hello<div class="${st(
+    classes.part,
+    cssStates({ myState: true })
+)}">Stylable</div>`;
