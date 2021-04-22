ede38d2419397a71f3199511def5354ea4ff0e9e
Barak Igal <baraki@wix.com>
2021-04-22 12:39:58 +0300

    add two more cases for sort-modules-by-depth

--- packages/build-tools/test/sort-modules-by-depth.spec.ts
+++ packages/build-tools/test/sort-modules-by-depth.spec.ts
@@ -2,7 +2,7 @@ import { sortModulesByDepth } from '@stylable/build-tools';
 import { expect } from 'chai';
 
 describe('sort-modules-by-depth', () => {
-    it('should sort by depth and name when depth conflicts', () => {
+    it('should sort by depth and name when depth conflicts (b0,a0,b1,a1)', () => {
         const sorted = sortModulesByDepth(
             [
                 { id: 'b0', depth: 0 },
@@ -21,6 +21,47 @@ describe('sort-modules-by-depth', () => {
             { id: 'b0', depth: 0 },
         ]);
     });
+
+    it('should sort by depth and name when depth conflicts (b1,b0,a0,a1)', () => {
+        const sorted = sortModulesByDepth(
+            [
+                { id: 'b1', depth: 1 },
+                { id: 'b0', depth: 0 },
+                { id: 'a0', depth: 0 },
+                { id: 'a1', depth: 1 },
+            ],
+            ({ depth }) => depth,
+            ({ id }) => id
+        );
+
+        expect(sorted).to.eql([
+            { id: 'a1', depth: 1 },
+            { id: 'b1', depth: 1 },
+            { id: 'a0', depth: 0 },
+            { id: 'b0', depth: 0 },
+        ]);
+    });
+
+    it('should sort by depth and name when depth conflicts (b0,b1,a1,a0)', () => {
+        const sorted = sortModulesByDepth(
+            [
+                { id: 'b0', depth: 0 },
+                { id: 'b1', depth: 1 },
+                { id: 'a1', depth: 1 },
+                { id: 'a0', depth: 0 },
+            ],
+            ({ depth }) => depth,
+            ({ id }) => id
+        );
+
+        expect(sorted).to.eql([
+            { id: 'a1', depth: 1 },
+            { id: 'b1', depth: 1 },
+            { id: 'a0', depth: 0 },
+            { id: 'b0', depth: 0 },
+        ]);
+    });
+
     it('should sort by depth and name when depth conflicts (reverse depth order)', () => {
         const sorted = sortModulesByDepth(
             [
