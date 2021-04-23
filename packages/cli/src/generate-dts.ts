0de94c4377b005d8370d765ede441d4f33a1a03a
Barak Igal <baraki@wix.com>
2021-04-23 22:28:38 +0300

    refactor

--- packages/cli/src/generate-dts.ts
+++ packages/cli/src/generate-dts.ts
@@ -1,83 +1,120 @@
-import { MappedStates, StylableResults } from '@stylable/core';
+import { StateParsedValue, StylableMeta, StylableResults } from '@stylable/core';
 
 const SPACING = ' '.repeat(4);
+const asString = (v: string) => JSON.stringify(v);
 
-function createStateEntries(states: MappedStates) {
-    return Object.entries(states)
-        .map(([stateName, stateDef]) =>
-            typeof stateDef !== 'string'
-                ? `${JSON.stringify(stateName)}?: ${
-                      stateDef === null
-                          ? 'boolean'
-                          : stateDef.type === 'enum'
-                          ? stateDef.arguments.map((v) => `${JSON.stringify(v)}`).join(' | ')
-                          : stateDef.type
-                  }`
-                : ''
-        )
-        .join(', ');
+function stringifyStringRecord(record: Record<string, string>, indent = SPACING) {
+    return Object.keys(record)
+        .map((k) => `${indent}${asString(k)}: string;`)
+        .join('\n');
 }
 
-export function createDTSContent(res: StylableResults) {
-    const { exports, meta: meta } = res;
-
-    const classesType = `declare type classesType = ${JSON.stringify(exports.classes, null, 4)};\n`;
-
-    const classes = 'export declare const classes: classesType;\n';
-
-    const namespace = `export declare const namespace = ${JSON.stringify(meta.namespace)};\n`;
-    const depth = `export declare const $depth: number;\n`;
-    const id = `export declare const $id: string | number;\n`;
-    const vars = `export declare const vars: {${Object.keys(exports.vars).map(
-        (v) => `${JSON.stringify(v)}: string`
-    ).join(', ')}}`;
-
-    const stVars = `export declare const stVars: ${JSON.stringify(exports.stVars, null, 4)};\n`;
-    const keyframes = `export declare const keyframes: ${JSON.stringify(
-        exports.keyframes,
-        null,
-        4
-    )};\n`;
-
-    const states = `type states = {
-${Object.entries(meta.classes)
-    .map(([name, cls]) =>
-        cls['-st-states']
-            ? `${SPACING}${JSON.stringify(`${exports.classes[name]}`)}: { ${createStateEntries(cls['-st-states'])} };`
-            : undefined
-    )
-    .filter(Boolean)
-    .join('\n')}
-} & { [key: string]: never };\n`;
-
-    const isString =
-        'type isString<T> = T extends string ? (string extends T ? true : false) : false;\n';
-
-    const functions = `export declare function cssStates<T extends keyof states>(s: states[T], ctx?: T | string): string;\n
-export declare function st<T extends string>(
-    ctx: T | undefined,
-    s?: string | (isString<T> extends false ? T extends keyof states ? states[T] : never : never),
-    ...rest: Array<string | undefined>
-): string;\n
-export declare const style: typeof st;\n`;
-
-    const emptyExport = `// do not remove - this is here to prevent auto-completion of inner parts
-export {};`;
-
-    return [
-        classesType,
-        classes,
-        namespace,
-        depth,
-        id,
-        vars,
-        stVars,
-        keyframes,
-        states,
-        isString,
-        functions,
-        emptyExport,
-    ]
-        .filter(Boolean)
+function stringifyClasses(classes: Record<string, string>, namespace: string, indent = SPACING) {
+    return Object.keys(classes)
+        .map((name) => `${indent}${asString(name)}: ${asString(scope(name, namespace))};`)
         .join('\n');
+}
+
+function stringifyStates({ classes, namespace }: StylableMeta) {
+    let out = '';
+    for (const [name, symbol] of Object.entries(classes)) {
+        const stStates = symbol['-st-states'];
+        if (!stStates) {
+            continue;
+        }
+        let statesEntries = ' ';
+        for (const [stateName, stateDef] of Object.entries(stStates)) {
+            if (typeof stateDef === 'string') {
+                continue;
+            }
+            statesEntries += `${asString(stateName)}?: ${getStateTSType(stateDef)}; `;
+        }
+        out += `${SPACING}${asString(scope(name, namespace))}: {${statesEntries}};\n`;
+    }
+    return out.slice(0, -1);
+}
+
+/**
+ * TODO: this function is not 100% correct and need a fix
+ * support custom validators in arguments?
+ * support custom types?
+ */
+function getStateTSType(stateDef: StateParsedValue | null) {
+    return stateDef === null
+        ? 'boolean'
+        : stateDef.type === 'enum'
+        ? stateDef.arguments
+              .map((v) => (typeof v === 'string' ? asString(v) : 'unknown'))
+              .join(' | ')
+        : stateDef.type /* what are the rest of the states */;
+}
+
+function wrapNL(code: string) {
+    return code ? `\n${code}\n` : code;
+}
+
+// TODO: make available from core currently defined in transformer class
+function scope(name: string, namespace: string, delimiter = '__') {
+    return namespace ? namespace + delimiter + name : name;
+}
+
+export function createDTSContent({ exports, meta }: StylableResults) {
+    const namespace = asString(meta.namespace);
+    const classes = wrapNL(stringifyClasses(exports.classes, meta.namespace));
+    const vars = wrapNL(stringifyStringRecord(exports.vars));
+    const stVars = wrapNL(stringifyStringRecord(exports.stVars));
+    const keyframes = wrapNL(stringifyStringRecord(exports.keyframes));
+    const states = wrapNL(stringifyStates(meta));
+
+    return `/* THIS FILE IS AUTO GENERATED DO NOT MODIFY */
+declare const namespace = ${namespace};
+
+type states = {${states}};
+
+declare const classes: {${classes}};
+
+declare const vars: {${vars}};
+
+declare const stVars: {${stVars}};
+
+declare const keyframes: {${keyframes}};
+
+declare function st<T extends string = keyof states>(
+    ctx: T | NullableString,
+    s?: GetStatesType<T>,
+    ...rest: NullableString[]
+): string;
+
+declare const style: typeof st;
+
+declare function cssStates<T = keyof states>(
+    s: T extends keyof states ? states[T] : never,
+    ctx?: T | string
+): string;
+
+export { 
+    classes,
+    vars,
+    stVars,
+    keyframes,
+    namespace,
+    st,
+    style,
+    cssStates
+};
+
+/* HELPERS */
+type NullableString = string | undefined | null;
+
+type isString<T> = T extends string ? (string extends T ? true : false) : false;
+
+type GetStatesType<T> = T extends string
+    ? isString<T> extends false
+        ? T extends keyof states
+            ? states[T] | NullableString
+            : NullableString
+        : NullableString
+    : NullableString;
+
+`;
 }
