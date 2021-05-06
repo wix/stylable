586d58c982d130f8269970063b956f1c11063cba
Barak Igal <baraki@wix.com>
2021-05-06 15:48:14 +0300

    add helper to exclude stylable from other loaders

--- packages/webpack-plugin/src/index.ts
+++ packages/webpack-plugin/src/index.ts
@@ -1,5 +1,6 @@
 export * from './plugin';
 export * from './plugin-utils';
 export * from './loader-utils';
+export * from './override-config';
 export * from './types';
 export * from './webpack-loader-types';
