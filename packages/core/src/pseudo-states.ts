f57f65fadc6f779c7975047860437766494277b0
Tzach Bonfil <45866571+tzachbon@users.noreply.github.com>
2021-08-02 18:00:05 +0300

    fix(core): assure that pseudo-classes warn when no arguments are given (#1962)

--- packages/core/src/pseudo-states.ts
+++ packages/core/src/pseudo-states.ts
@@ -21,6 +21,8 @@ export const stateErrors = {
         `pseudo-state "${name}" defined with unknown type: "${type}"`,
     TOO_MANY_STATE_TYPES: (name: string, types: string[]) =>
         `pseudo-state "${name}(${types.join(', ')})" definition must be of a single type`,
+    NO_STATE_ARGUMENT_GIVEN: (name: string, type: string) =>
+        `pseudo-state "${name}" expected argument of type "${type}" but got none`,
     NO_STATE_TYPE_GIVEN: (name: string) =>
         `pseudo-state "${name}" expected a definition of a single type, but received none`,
     TOO_MANY_ARGS_IN_VALIDATOR: (name: string, validator: string, args: string[]) =>
@@ -294,6 +296,12 @@ function resolveStateValue(
         rule,
         node.content || stateDef.defaultValue
     );
+
+    if (rule && !node.content && !stateDef.defaultValue) {
+        diagnostics.warn(rule, stateErrors.NO_STATE_ARGUMENT_GIVEN(name, stateDef.type), {
+            word: name,
+        });
+    }
 
     const validator = systemValidators[stateDef.type];
 
