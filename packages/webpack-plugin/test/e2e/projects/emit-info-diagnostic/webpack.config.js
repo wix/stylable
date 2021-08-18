53412a7a8103dfe3748bee07533aaffd80c90d5e
Tzach Bonfil <45866571+tzachbon@users.noreply.github.com>
2021-08-18 17:30:58 +0300

    test info diagnostic

---
+++ packages/webpack-plugin/test/e2e/projects/emit-info-diagnostic/webpack.config.js
@@ -0,0 +1,24 @@
+const { StylableWebpackPlugin } = require('@stylable/webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+    mode: 'development',
+    context: __dirname,
+    devtool: 'source-map',
+    plugins: [
+        new StylableWebpackPlugin({
+            diagnosticsMode: 'strict',
+            stylableConfig: (config) => ({
+                ...config,
+                hooks: {
+                    postProcessor: (result) => {
+                        result.meta.diagnostics.info(result.meta.ast.root(), 'Test info waring!');
+                        return result;
+                    },
+                },
+            }),
+        }),
+        new HtmlWebpackPlugin(),
+    ],
+};
