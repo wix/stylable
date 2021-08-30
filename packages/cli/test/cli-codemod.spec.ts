32aef719dd2004bb79dc08445a5b0779fd73c861
Tzach Bonfil <45866571+tzachbon@users.noreply.github.com>
2021-08-30 12:02:03 +0300

    test: add alias use case

--- packages/cli/test/cli-codemod.spec.ts
+++ packages/cli/test/cli-codemod.spec.ts
@@ -29,7 +29,7 @@ describe('Stylable Cli Code Mods', () => {
         it('handle all named cases', () => {
             populateDirectorySync(tempDir.path, {
                 'package.json': `{"name": "test", "version": "0.0.0"}`,
-                'style.st.css': `:import {-st-from: './x'; -st-default: Name; -st-named: name1, name2, keyframes(kf1, kf2), keyframes(kf3, kf4), --var1}`,
+                'style.st.css': `:import {-st-from: './x'; -st-default: Name; -st-named: name1 as name1Alias, name2, keyframes(kf1 as kf1Alias, kf2), keyframes(kf3, kf4), --var1}`,
             });
 
             runCliCodeMod(['--rootDir', tempDir.path, '--mods', 'st-import-to-at-import']);
@@ -37,7 +37,7 @@ describe('Stylable Cli Code Mods', () => {
             const dirContent = loadDirSync(tempDir.path);
 
             expect(dirContent['style.st.css']).equal(
-                '@st-import Name, [name1, name2, --var1, keyframes(kf1, kf2, kf3, kf4)] from "./x";'
+                '@st-import Name, [name1 as name1Alias, name2, --var1, keyframes(kf1 as kf1Alias, kf2, kf3, kf4)] from "./x";'
             );
         });
     });
