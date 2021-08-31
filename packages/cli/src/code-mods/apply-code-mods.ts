eeef1258bde178e692b8aef6eae485dfea7ce5a5
Tzach Bonfil <45866571+tzachbon@users.noreply.github.com>
2021-08-31 17:57:48 +0300

    fix: handle parse failure

--- packages/cli/src/code-mods/apply-code-mods.ts
+++ packages/cli/src/code-mods/apply-code-mods.ts
@@ -1,5 +1,5 @@
 import { Diagnostic, Diagnostics } from '@stylable/core';
-import { Root, parse } from 'postcss';
+import { Root, parse, CssSyntaxError } from 'postcss';
 import { stImportToAtImport } from './st-import-to-at-import';
 
 export type CodeMod = (ast: Root, diagnostics: Diagnostics) => void;
@@ -8,9 +8,42 @@ export const registeredMods: Map<string, CodeMod> = new Map([
     ['st-import-to-at-import', stImportToAtImport],
 ]);
 
-export function applyCodeMods(css: string, mods: Set<{ id: string; apply: CodeMod }>) {
+interface ApplyCodeModsFailure {
+    type: 'failure';
+    error: CssSyntaxError | Error;
+}
+
+interface ApplyCodeModsSuccess {
+    type: 'success';
+    css: string;
+    reports: Map<string, Diagnostic[]>;
+}
+
+type ApplyCodeModsResult = ApplyCodeModsSuccess | ApplyCodeModsFailure;
+
+export function applyCodeMods(
+    css: string,
+    mods: Set<{ id: string; apply: CodeMod }>
+): ApplyCodeModsResult {
     const reports = new Map<string, Diagnostic[]>();
-    const ast = parse(css);
+    let ast: Root;
+
+    try {
+        ast = parse(css);
+    } catch (error) {
+        if (error instanceof CssSyntaxError || error instanceof Error) {
+            return {
+                type: 'failure',
+                error,
+            };
+        } else {
+            return {
+                type: 'failure',
+                error: new Error(String(error)),
+            };
+        }
+    }
+
     for (const { id, apply } of mods) {
         const diagnostics = new Diagnostics();
 
@@ -20,8 +53,8 @@ export function applyCodeMods(css: string, mods: Set<{ id: string; apply: CodeMo
             reports.set(id, diagnostics.reports);
         }
     }
-
     return {
+        type: 'success',
         css: ast.toString(),
         reports,
     };
--- packages/cli/test/cli-codemod.spec.ts
+++ packages/cli/test/cli-codemod.spec.ts
@@ -26,6 +26,26 @@ describe('Stylable Cli Code Mods', () => {
         expect(dirContent['style.st.css']).equal('@st-import Name from "./x";');
     });
 
+    it('should handle parse failure', () => {
+        populateDirectorySync(tempDir.path, {
+            'package.json': `{"name": "test", "version": "0.0.0"}`,
+            'style.st.css': `:import {-st-from: './x' -st-default: Name}`, // missing semi column
+        });
+
+        const { stdout } = runCliCodeMod([
+            '--rootDir',
+            tempDir.path,
+            '--mods',
+            'st-import-to-at-import',
+        ]);
+
+        const dirContent = loadDirSync(tempDir.path);
+
+        expect(dirContent['style.st.css']).equal(`:import {-st-from: './x' -st-default: Name}`);
+        expect(stdout).to.match(/failed to parse/);
+        expect(stdout).to.match(/CssSyntaxError: <css input>:1:20: Missed semicolon/);
+    });
+
     describe('st-import-to-at-import', () => {
         it('handle all named cases', () => {
             populateDirectorySync(tempDir.path, {
