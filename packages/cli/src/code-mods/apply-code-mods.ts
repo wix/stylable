e68ba57281ba39bc53813bae11741baaeb961a5f
Tzach Bonfil <45866571+tzachbon@users.noreply.github.com>
2021-09-05 12:54:35 +0300

    feat(cli): use stylable diagnostics in the code-mod CLI (#2036)

--- packages/cli/src/code-mods/apply-code-mods.ts
+++ packages/cli/src/code-mods/apply-code-mods.ts
@@ -1,24 +1,60 @@
-import { Root, parse } from 'postcss';
+import { Diagnostic, Diagnostics } from '@stylable/core';
+import { Root, parse, CssSyntaxError } from 'postcss';
 import { stImportToAtImport } from './st-import-to-at-import';
 
-export type CodeMod = (ast: Root, messages: string[]) => void;
+export type CodeMod = (ast: Root, diagnostics: Diagnostics) => void;
 
 export const registeredMods: Map<string, CodeMod> = new Map([
     ['st-import-to-at-import', stImportToAtImport],
 ]);
 
-export function applyCodeMods(css: string, mods: Set<{ id: string; apply: CodeMod }>) {
-    const reports = new Map<string, string[]>();
-    const ast = parse(css);
-    for (const { id, apply } of mods) {
-        const messages: string[] = [];
-        apply(ast, messages);
-        if (messages.length) {
-            reports.set(id, messages);
+interface ApplyCodeModsFailure {
+    type: 'failure';
+    error: CssSyntaxError | Error;
+}
+
+interface ApplyCodeModsSuccess {
+    type: 'success';
+    css: string;
+    reports: Map<string, Diagnostic[]>;
+}
+
+type ApplyCodeModsResult = ApplyCodeModsSuccess | ApplyCodeModsFailure;
+
+export function applyCodeMods(
+    css: string,
+    mods: Set<{ id: string; apply: CodeMod }>
+): ApplyCodeModsResult {
+    const reports = new Map<string, Diagnostic[]>();
+    let ast: Root;
+
+    try {
+        ast = parse(css);
+    } catch (error) {
+        if (error instanceof CssSyntaxError || error instanceof Error) {
+            return {
+                type: 'failure',
+                error,
+            };
+        } else {
+            return {
+                type: 'failure',
+                error: new Error(String(error)),
+            };
         }
     }
 
+    for (const { id, apply } of mods) {
+        const diagnostics = new Diagnostics();
+
+        apply(ast, diagnostics);
+
+        if (diagnostics.reports.length) {
+            reports.set(id, diagnostics.reports);
+        }
+    }
     return {
+        type: 'success',
         css: ast.toString(),
         reports,
     };
