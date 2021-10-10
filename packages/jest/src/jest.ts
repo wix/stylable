ad84368fe4965fa47843e53dc2484a0969f6e444
Tom Raviv <tomr@wix.com>
2021-10-10 16:18:20 +0300

    fix(jest): enable default package namespace resolver fix(jest): enable stylable configuration through jest config

--- packages/jest/src/jest.ts
+++ packages/jest/src/jest.ts
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import type { StylableConfig } from '@stylable/core';
-import { Options, stylableModuleFactory } from '@stylable/module-utils';
+import { stylableModuleFactory } from '@stylable/module-utils';
+import { resolveNamespace } from '@stylable/node';
 
 const stylableRuntimePath = require.resolve('@stylable/runtime');
 const stylableCorePath = require.resolve('@stylable/core');
@@ -9,26 +10,7 @@ const { version: runtimeVersion } = require('@stylable/runtime/package.json') as
 };
 const { version: coreVersion } = require('@stylable/core/package.json') as { version: string };
 
-export function processFactory(
-    stylableConfig?: Partial<StylableConfig>,
-    factoryOptions?: Partial<Options>
-) {
-    return stylableModuleFactory(
-        {
-            fileSystem: fs,
-            requireModule: require,
-            projectRoot: '',
-            ...stylableConfig,
-        },
-        // ensure the generated module points to our own @stylable/runtime copy
-        // this allows @stylable/jest to be used as part of a globally installed CLI
-        { runtimePath: stylableRuntimePath, ...factoryOptions }
-    );
-}
-
-export const process = processFactory();
-
-export function getCacheKey(
+function getCacheKey(
     fileData: string,
     filename: string,
     configString: string,
@@ -45,3 +27,23 @@ export function getCacheKey(
         coreVersion
     );
 }
+
+export const createTransformer = (options?: Partial<StylableConfig>) => {
+    // ...
+    return {
+        process: stylableModuleFactory(
+            {
+                fileSystem: fs,
+                requireModule: require,
+                projectRoot: '',
+                resolveNamespace,
+                ...options,
+            },
+            // ensure the generated module points to our own @stylable/runtime copy
+            // this allows @stylable/jest to be used as part of a globally installed CLI
+            { runtimePath: stylableRuntimePath }
+        ),
+        getCacheKey,
+        canInstrument: false,
+    };
+};
--- packages/jest/test/jest.spec.ts
+++ packages/jest/test/jest.spec.ts
@@ -1,16 +1,34 @@
 import { expect } from 'chai';
 import { readFileSync } from 'fs';
 import nodeEval from 'node-eval';
+import { createTransformer } from '@stylable/jest';
 import type { RuntimeStylesheet } from '@stylable/runtime';
-import { process } from '@stylable/jest';
 
 describe('jest process', () => {
-    it('should process stylable sources', () => {
+    it('should process stylable sources using createTransformer API', () => {
         const filename = require.resolve('@stylable/jest/test/fixtures/test.st.css');
         const content = readFileSync(filename, 'utf8');
-        const module = nodeEval(process(content, filename), filename) as RuntimeStylesheet;
+        const module = nodeEval(
+            createTransformer().process(content, filename),
+            filename
+        ) as RuntimeStylesheet;
 
         expect(module.classes.root).to.equal(`${module.namespace}__root`);
         expect(module.classes.test).to.equal(`${module.namespace}__test`);
+    });
+
+    it('should process stylable sources with a custom namespace resolver', () => {
+        const filename = require.resolve('@stylable/jest/test/fixtures/test.st.css');
+        const content = readFileSync(filename, 'utf8');
+        const module = nodeEval(
+            createTransformer({ resolveNamespace: (ns, _srcPath) => `${ns}-custom` }).process(
+                content,
+                filename
+            ),
+            filename
+        ) as RuntimeStylesheet;
+
+        expect(module.classes.root).to.equal(`test-custom__root`);
+        expect(module.classes.test).to.equal(`test-custom__test`);
     });
 });
