890c5b4a5aaaabd6cc99c71dda9e058d31a0a8b0
Barak Igal <baraki@wix.com>
2021-11-03 14:18:42 +0200

    refactor: remove deprecation logs from matchSelectorTarget and createSubsetAst

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -235,9 +235,7 @@ export const scopeSelector = wrapFunctionForDeprecation(deprecatedScopeSelector,
     name: `scopeSelector`,
 });
 /**@deprecated*/
-export const createSubsetAst = wrapFunctionForDeprecation(deprecatedCreateSubsetAst, {
-    name: `createSubsetAst`,
-});
+export const createSubsetAst = deprecatedCreateSubsetAst;
 /**@deprecated*/
 export const removeUnusedRules = wrapFunctionForDeprecation(deprecatedRemoveUnusedRules, {
     name: `removeUnusedRules`,
@@ -276,9 +274,7 @@ import {
     createSimpleSelectorChecker as deprecatedCreateSimpleSelectorChecker,
 } from './deprecated/deprecated-selector-utils';
 /**@deprecated*/
-export const matchSelectorTarget = wrapFunctionForDeprecation(deprecatedMatchSelectorTarget, {
-    name: `matchSelectorTarget`,
-});
+export const matchSelectorTarget = deprecatedMatchSelectorTarget;
 /**@deprecated*/
 export const fixChunkOrdering = wrapFunctionForDeprecation(deprecatedFixChunkOrdering, {
     name: `fixChunkOrdering`,
