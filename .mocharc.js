ffa85b768b03f4a8c2938addf9f133db4bb2722f
Tzach Bonfil <45866571+tzachbon@users.noreply.github.com>
2021-11-04 13:54:53 +0200

    fix: decrease the minimum

--- .mocharc.js
+++ .mocharc.js
@@ -3,5 +3,5 @@ const os = require('os');
 module.exports = {
     colors: true,
     'enable-source-maps': true,
-    jobs: Math.min(4, os.cpus().length - 1),
+    jobs: Math.min(2, os.cpus().length - 1),
 };
