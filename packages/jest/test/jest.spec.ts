500b6d66111c26965b09080f98d7eb2ceaa0235e
Tom Raviv <tomr@wix.com>
2021-11-14 13:04:09 +0200

    fix(jest): correct export to match jest api expectation

--- packages/jest/test/jest.spec.ts
+++ packages/jest/test/jest.spec.ts
@@ -1,15 +1,17 @@
 import { expect } from 'chai';
 import { readFileSync } from 'fs';
 import nodeEval from 'node-eval';
-import { createTransformer } from '@stylable/jest';
+import stylableTransformer from '@stylable/jest';
 import type { RuntimeStylesheet } from '@stylable/runtime';
 
 describe('jest process', () => {
     it('should process stylable sources using createTransformer API', () => {
         const filename = require.resolve('@stylable/jest/test/fixtures/test.st.css');
         const content = readFileSync(filename, 'utf8');
+        const transformer = stylableTransformer.createTransformer();
+
         const module = nodeEval(
-            createTransformer().process(content, filename),
+            transformer.process(content, filename),
             filename
         ) as RuntimeStylesheet;
 
@@ -20,10 +22,12 @@ describe('jest process', () => {
     it('should process stylable sources with a custom namespace resolver', () => {
         const filename = require.resolve('@stylable/jest/test/fixtures/test.st.css');
         const content = readFileSync(filename, 'utf8');
+        const transformer = stylableTransformer.createTransformer({
+            stylable: { resolveNamespace: (ns, _srcPath) => `${ns}-custom` },
+        });
+
         const module = nodeEval(
-            createTransformer({
-                stylable: { resolveNamespace: (ns, _srcPath) => `${ns}-custom` },
-            }).process(content, filename),
+            transformer.process(content, filename),
             filename
         ) as RuntimeStylesheet;
 
