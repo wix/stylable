fbc401ab4280d4767ad8e303b4730a847e8eeba4
Ido Rosenthal <idoros@gmail.com>
2021-12-14 11:40:29 +0200

    feat: STSymbol setup to manage all symbols
    - single place to register symbols / namespaces
    - refactored mixin inherit meta to handle new data structure
    - add new dev dep `ts-expect` to test types

--- packages/core/src/features/st-symbol.ts
+++ packages/core/src/features/st-symbol.ts
@@ -8,6 +8,9 @@ import { ignoreDeprecationWarn } from '../helpers/deprecation';
 import type { StylableMeta } from '../stylable-meta';
 import type * as postcss from 'postcss';
 
+// SYMBOLS DEFINITION
+
+// union of all of the symbols
 export type StylableSymbol =
     | ImportSymbol
     | VarSymbol
@@ -15,8 +18,71 @@ export type StylableSymbol =
     | ElementSymbol
     | CSSVarSymbol
     | KeyframesSymbol;
+// the namespace that each symbol exists on
+const NAMESPACES: Record<SymbolTypes, `main` | `keyframes`> = {
+    import: `main`,
+    class: `main`,
+    cssVar: `main`,
+    element: `main`,
+    keyframes: `keyframes`,
+    var: `main`,
+};
+// state structure
+function createState(clone?: State): State {
+    return {
+        byNS: {
+            main: clone ? [...clone.byNS.main] : [],
+            keyframes: clone ? [...clone.byNS.keyframes] : [],
+        },
+        byNSFlat: {
+            main: clone ? { ...clone.byNSFlat.main } : {},
+            keyframes: clone ? { ...clone.byNSFlat.keyframes } : {},
+        },
+        byType: {
+            import: clone ? { ...clone.byType.import } : {},
+            class: clone ? { ...clone.byType.class } : {},
+            cssVar: clone ? { ...clone.byType.cssVar } : {},
+            element: clone ? { ...clone.byType.element } : {},
+            keyframes: clone ? { ...clone.byType.keyframes } : {},
+            var: clone ? { ...clone.byType.var } : {},
+        },
+    };
+}
 
-const dataKey = plugableRecord.key<Record<string, StylableSymbol>>('mappedSymbols');
+// internal types
+type SymbolTypes = StylableSymbol['_kind'];
+type Namespaces = typeof NAMESPACES;
+interface SymbolDeclaration<K = Namespaces[SymbolTypes]> {
+    name: string;
+    symbol: Extract<
+        StylableSymbol,
+        {
+            _kind: SymbolTypes extends any
+                ? Namespaces[SymbolTypes] extends K
+                    ? SymbolTypes
+                    : any
+                : any;
+        }
+    >;
+    ast: postcss.Node | undefined;
+    safeRedeclare: boolean; // ToDo: change to action: `def` | `ref` | `final`?
+}
+interface State {
+    byNS: {
+        [K in Namespaces[SymbolTypes]]: SymbolDeclaration<K>[];
+    };
+    byNSFlat: {
+        [K in Namespaces[SymbolTypes]]: Record<
+            string,
+            StylableSymbol // ToDo: filter only relevant types to ns
+        >;
+    };
+    byType: {
+        [K in SymbolTypes]: Record<string, Extract<StylableSymbol, { _kind: K }>>;
+    };
+}
+
+const dataKey = plugableRecord.key<State>('mappedSymbols');
 
 export const diagnostics = {
     REDECLARE_SYMBOL(name: string) {
@@ -28,19 +94,39 @@ export const diagnostics = {
 
 export const hooks = createFeature({
     metaInit({ meta }) {
-        plugableRecord.set(meta.data, dataKey, {});
+        plugableRecord.set(meta.data, dataKey, createState());
     },
 });
 
 // API
 
-export function get(meta: StylableMeta, name: string): StylableSymbol | undefined {
-    const state = plugableRecord.getUnsafe(meta.data, dataKey);
-    return state[name];
+export function get<T extends SymbolTypes>(
+    meta: StylableMeta,
+    name: string,
+    type?: T
+): Extract<StylableSymbol, { _kind: T }> | undefined {
+    const { byNSFlat, byType } = plugableRecord.getUnsafe(meta.data, dataKey);
+    if (type) {
+        return byType[type][name] as any; // ToDo: try improve types
+    }
+    const nsName = type ? NAMESPACES[type] : `main`;
+    return byNSFlat[nsName][name] as any;
 }
 
-export function getAll(meta: StylableMeta): Record<string, StylableSymbol> {
-    return plugableRecord.getUnsafe(meta.data, dataKey);
+export function getAll(
+    meta: StylableMeta,
+    ns: Namespaces[SymbolTypes] = `main`
+): Record<string, StylableSymbol> {
+    const { byNSFlat } = plugableRecord.getUnsafe(meta.data, dataKey);
+    return byNSFlat[ns];
+}
+
+export function getAllByType<T extends SymbolTypes>(
+    meta: StylableMeta,
+    type: T
+): State['byType'][T] {
+    const { byType } = plugableRecord.getUnsafe(meta.data, dataKey);
+    return byType[type];
 }
 
 export function addSymbol({
@@ -56,22 +142,43 @@ export function addSymbol({
     safeRedeclare?: boolean;
     localName?: string;
 }) {
-    const stSymbolData = plugableRecord.getUnsafe(context.meta.data, dataKey);
+    const { byNS, byNSFlat, byType } = plugableRecord.getUnsafe(context.meta.data, dataKey);
     const name = localName || symbol.name;
-    const existingSymbol = stSymbolData[name];
+    const typeTable = byType[symbol._kind];
+    const nsName = NAMESPACES[symbol._kind];
+    const existingSymbol = byNS[nsName].find(({ name: existingName }) => name === existingName);
     if (existingSymbol && node && !safeRedeclare) {
         context.diagnostics.warn(node, diagnostics.REDECLARE_SYMBOL(name), {
             word: name,
         });
     }
-    stSymbolData[name] = symbol;
+    byNS[nsName].push({ name, symbol, ast: node, safeRedeclare });
+    byNSFlat[nsName][name] = symbol;
+    typeTable[name] = symbol;
     // deprecated
     ignoreDeprecationWarn(() => {
         context.meta.mappedSymbols[name] = symbol;
     });
 }
 
+/* inheritSymbols/forceSetSymbol are used for creating a copy meta with mixin root */
 export function inheritSymbols(originMeta: StylableMeta, targetMeta: StylableMeta) {
     const originData = plugableRecord.getUnsafe(originMeta.data, dataKey);
-    plugableRecord.set(targetMeta.data, dataKey, Object.create(originData));
+    plugableRecord.set(targetMeta.data, dataKey, createState(originData));
+}
+export function forceSetSymbol({
+    meta,
+    symbol,
+    localName,
+}: {
+    meta: StylableMeta;
+    symbol: StylableSymbol;
+    localName?: string;
+}) {
+    const { byNS, byNSFlat, byType } = plugableRecord.getUnsafe(meta.data, dataKey);
+    const name = localName || symbol.name;
+    byNS.main.push({ name, symbol, safeRedeclare: false, ast: undefined });
+    byNSFlat.main[name] = symbol;
+    byType[symbol._kind][name] = symbol;
+    // ToDo: maybe override type record according to symbol._kind
 }
--- packages/core/test/features/st-symbol.spec.ts
+++ packages/core/test/features/st-symbol.spec.ts
@@ -1,9 +1,16 @@
-import { STSymbol, StylableSymbol } from '@stylable/core/dist/features';
+import {
+    STSymbol,
+    StylableSymbol,
+    ClassSymbol,
+    ElementSymbol,
+    CSSVarSymbol,
+} from '@stylable/core/dist/features';
 import { ignoreDeprecationWarn } from '@stylable/core/dist/helpers/deprecation';
 import { Diagnostics } from '@stylable/core/dist/diagnostics';
 import { generateStylableResult } from '@stylable/core-test-kit';
 import * as postcss from 'postcss';
 import { expect } from 'chai';
+import { expectType, TypeEqual } from 'ts-expect';
 
 describe(`features/st-symbol`, () => {
     describe(`meta`, () => {
@@ -22,8 +29,11 @@ describe(`features/st-symbol`, () => {
 
             STSymbol.addSymbol({ context, symbol });
 
-            expect(STSymbol.get(meta, `a`)).to.equal(symbol);
+            const savedSymbol = STSymbol.get(meta, `a`);
+            expect(savedSymbol).to.equal(symbol);
             expect(meta.getSymbol(`a`), `meta.getSymbol`).to.equal(STSymbol.get(meta, `a`));
+            expectType<TypeEqual<StylableSymbol | undefined, typeof savedSymbol>>(true);
+            expectType<TypeEqual<ClassSymbol | undefined, typeof savedSymbol>>(false);
             // deprecation
             expect(
                 ignoreDeprecationWarn(() => meta.mappedSymbols),
@@ -32,6 +42,33 @@ describe(`features/st-symbol`, () => {
                 root: STSymbol.get(meta, `root`),
                 a: STSymbol.get(meta, `a`),
             });
+        });
+        it(`should keep track of symbols by type`, () => {
+            const { meta } = generateStylableResult({
+                entry: `/entry.st.css`,
+                files: {
+                    '/entry.st.css': {
+                        namespace: `entry`,
+                        content: ``,
+                    },
+                },
+            });
+            const classSymbol: StylableSymbol = { _kind: `class`, name: `a` };
+            const typeSymbol: StylableSymbol = { _kind: `element`, name: `a` };
+            const context = { meta, diagnostics: new Diagnostics() };
+
+            STSymbol.addSymbol({ context, symbol: classSymbol });
+            STSymbol.addSymbol({ context, symbol: typeSymbol });
+
+            const savedClass = STSymbol.get(meta, `a`, `class`);
+            const savedType = STSymbol.get(meta, `a`, `element`);
+            const savedVar = STSymbol.get(meta, `a`, `cssVar`);
+            expect(savedClass, `class`).to.equal(classSymbol);
+            expect(savedType, `element`).to.equal(typeSymbol);
+            expect(savedVar, `var - not registered`).to.equal(undefined);
+            expectType<TypeEqual<ClassSymbol | undefined, typeof savedClass>>(true);
+            expectType<TypeEqual<ElementSymbol | undefined, typeof savedType>>(true);
+            expectType<TypeEqual<CSSVarSymbol | undefined, typeof savedVar>>(true);
         });
         it(`should override previous symbol`, () => {
             const { meta } = generateStylableResult({
@@ -50,7 +87,9 @@ describe(`features/st-symbol`, () => {
             STSymbol.addSymbol({ context, symbol: symbolA });
             STSymbol.addSymbol({ context, symbol: symbolB });
 
-            expect(STSymbol.get(meta, `a`), `override`).to.equal(symbolB);
+            const savedType = STSymbol.get(meta, `a`);
+            expect(savedType, `override`).to.equal(symbolB);
+            expectType<TypeEqual<StylableSymbol | undefined, typeof savedType>>(true);
         });
         it(`should return collected symbols`, () => {
             const { meta } = generateStylableResult({
@@ -72,6 +111,35 @@ describe(`features/st-symbol`, () => {
                 Gallery: STSymbol.get(meta, `Gallery`),
             });
             expect(meta.getAllSymbols(), `meta.getAllSymbols`).to.eql(STSymbol.getAll(meta));
+        });
+        it(`should return collected symbols by namespace`, () => {
+            const { meta } = generateStylableResult({
+                entry: `/entry.st.css`,
+                files: {
+                    '/entry.st.css': {
+                        namespace: `entry`,
+                        content: ``,
+                    },
+                },
+            });
+            const classSymbol: StylableSymbol = { _kind: `class`, name: `a` };
+            const typeSymbol: StylableSymbol = { _kind: `element`, name: `b` };
+            const keyframesSymbol: StylableSymbol = { _kind: `keyframes`, name: `c`, alias: `` };
+            const context = { meta, diagnostics: new Diagnostics() };
+
+            STSymbol.addSymbol({ context, symbol: classSymbol });
+            STSymbol.addSymbol({ context, symbol: typeSymbol });
+            STSymbol.addSymbol({ context, symbol: keyframesSymbol });
+
+            expect(STSymbol.getAll(meta), `default to main ns`).to.eql({
+                root: STSymbol.get(meta, `root`),
+                a: STSymbol.get(meta, `a`),
+                b: STSymbol.get(meta, `b`),
+            });
+            expect(STSymbol.getAll(meta, `main`), `main`).to.eql(STSymbol.getAll(meta));
+            expect(STSymbol.getAll(meta, `keyframes`), `keyframes`).to.eql({
+                c: STSymbol.get(meta, `c`, `keyframes`),
+            });
         });
         it(`should accept optional local name different then symbol name`, () => {
             const { meta } = generateStylableResult({
