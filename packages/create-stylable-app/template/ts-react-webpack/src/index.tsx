f0cc6240e7d5909531ffe813cbf9b4219f9231fe
Tzach Bonfil <45866571+tzachbon@users.noreply.github.com>
2021-12-20 12:05:24 +0200

    feat: relocate src files

--- packages/create-stylable-app/template/ts-react-webpack/src/index.tsx
+++ packages/create-stylable-app/template/ts-react-webpack/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './components/app';
-import './styles/globals.st.css';
-import './styles/reset.st.css';
+import { App } from './app';
+import './globals.st.css';
+import './reset.st.css';
 
 ReactDOM.render(
     <React.StrictMode>
