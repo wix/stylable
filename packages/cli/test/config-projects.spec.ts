4a752c10be1eb3ce8b121ff503edcde7e5c40e0e
Tzach Bonfil <45866571+tzachbon@users.noreply.github.com>
2021-12-20 16:28:02 +0200

    remove only...

--- packages/cli/test/config-projects.spec.ts
+++ packages/cli/test/config-projects.spec.ts
@@ -281,7 +281,7 @@ describe('Stylable CLI config multiple projects', function () {
             expect(stdout, 'has diagnostic error').not.to.match(/error/i);
         });
 
-        it.only('should prioritize build order by projects specification', () => {
+        it('should prioritize build order by projects specification', () => {
             populateDirectorySync(tempDir.path, {
                 'package.json': JSON.stringify({
                     name: 'workspace',
