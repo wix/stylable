ecf530fef1d65100b4414ea6c4e2f89d8d8b294b
Ido Rosenthal <idoros@gmail.com>
2022-01-10 09:14:58 +0200

    test: suppor multiple files as input to test

--- packages/core-test-kit/src/test-stylable-core.ts
+++ packages/core-test-kit/src/test-stylable-core.ts
@@ -1,26 +1,52 @@
 import { testInlineExpects } from './inline-expectation';
 import { Stylable, StylableExports, StylableMeta } from '@stylable/core';
 import { createMemoryFs } from '@file-services/memory';
+import type { IDirectoryContents } from '@file-services/types';
+import { isAbsolute } from 'path';
 
-export function testStylableCore(input: string) {
+export interface TestOptions {
+    entries: string[];
+}
+export function testStylableCore(
+    input: string | IDirectoryContents,
+    options: Partial<TestOptions> = {}
+) {
     // infra
-    const fs = createMemoryFs({ '/entry.st.css': input });
+    const fs = createMemoryFs(typeof input === `string` ? { '/entry.st.css': input } : input);
     const stylable = Stylable.create({
         fileSystem: fs,
         projectRoot: '/',
         resolveNamespace: (ns) => ns,
     });
 
+    // collect sheets
+    const allSheets = fs.findFilesSync(`/`, { filterFile: ({ path }) => path.endsWith(`.st.css`) });
+    const entries = options.entries || allSheets;
+
     // transform entries
     const sheets: Record<string, { meta: StylableMeta; exports: StylableExports }> = {};
-    const path = '/entry.st.css';
-    const meta = stylable.process(path);
-    const { exports } = stylable.transform(meta);
-    sheets[path] = { meta, exports };
+    for (const path of entries) {
+        if (!isAbsolute(path || '')) {
+            throw new Error(testStylableCore.errors.absoluteEntry(path));
+        }
+        const meta = stylable.process(path);
+        const { exports } = stylable.transform(meta);
+        sheets[path] = { meta, exports };
+    }
 
     // inline test
-    testInlineExpects({ meta });
+    for (const path of allSheets) {
+        const meta = stylable.process(path);
+        if (!meta.outputAst) {
+            // ToDo: test
+            stylable.transform(meta);
+        }
+        testInlineExpects({ meta });
+    }
 
     // expose infra and entry sheets
     return { sheets, stylable, fs };
 }
+testStylableCore.errors = {
+    absoluteEntry: (entry: string) => `entry must be absolute path got: ${entry}`,
+};
--- packages/core-test-kit/test/test-stylable-core.spec.ts
+++ packages/core-test-kit/test/test-stylable-core.spec.ts
@@ -44,4 +44,79 @@ describe(`testStylableCore()`, () => {
 
         expect(newMeta.outputAst?.toString().trim()).to.equal(`.entry__part {}`);
     });
+    describe(`multiple files`, () => {
+        it(`should accept a multiple files (transform all by default)`, () => {
+            const { sheets } = testStylableCore({
+                '/a.st.css': ``,
+                '/b.st.css': ``,
+            });
+
+            const a = sheets[`/a.st.css`];
+            const b = sheets[`/b.st.css`];
+
+            expect(CSSClass.get(a.meta, `root`), `a meta`).to.contain({ name: `root` });
+            expect(a.exports.classes.root, `a exports`).equal(`a__root`);
+            expect(CSSClass.get(b.meta, `root`), `b meta`).to.contain({ name: `root` });
+            expect(b.exports.classes.root, `b exports`).equal(`b__root`);
+        });
+        it(`should accept entries to transform`, () => {
+            const { sheets } = testStylableCore(
+                {
+                    '/entry.st.css': ``,
+                    '/a.st.css': ``,
+                    '/b.st.css': ``,
+                },
+                {
+                    entries: [`/a.st.css`, `/b.st.css`],
+                }
+            );
+
+            const entryNotProcessed = sheets[`/entry.st.css`];
+            const a = sheets[`/a.st.css`];
+            const b = sheets[`/b.st.css`];
+
+            expect(entryNotProcessed, `entry not processed`).to.equal(undefined);
+            expect(a.exports.classes.root, `b exports.classes`).to.equal(`a__root`);
+            expect(b.exports.classes.root, `b exports.classes`).to.equal(`b__root`);
+        });
+        it(`should throw on none absolute path entry`, () => {
+            expect(() =>
+                testStylableCore(
+                    {
+                        '/a.st.css': ``,
+                    },
+                    {
+                        entries: [`a.st.css`],
+                    }
+                )
+            ).to.throw(testStylableCore.errors.absoluteEntry(`a.st.css`));
+        });
+        it(`should inline test all files (even these that are not linked to entries)`, () => {
+            expect(() =>
+                testStylableCore({
+                    '/entry.st.css': `
+                        /* @rule .entry__part */
+                        .part {}
+                    `,
+                    '/other.st.css': `
+                        /* @rule .other__part */
+                        .part {}
+                    `,
+                })
+            ).to.not.throw();
+
+            expect(() =>
+                testStylableCore({
+                    '/entry.st.css': `
+                        /* @rule .entry__part */
+                        .part {}
+                    `,
+                    '/other.st.css': `
+                        /* @rule .fail__part */
+                        .part {}
+                    `,
+                })
+            ).to.throw();
+        });
+    });
 });
