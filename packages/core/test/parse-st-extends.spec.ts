d9b9bf66d1d46913f0ab9e3bf308b46147c97bf6
Barak Igal <baraki@wix.com>
2022-01-31 13:43:11 +0200

    refactor: added error cases tests

--- packages/core/test/parse-st-extends.spec.ts
+++ packages/core/test/parse-st-extends.spec.ts
@@ -1,6 +1,8 @@
 import { expect } from 'chai';
 import { parseCSSValue } from '@tokey/css-value-parser';
 
+type ParsedNodes = ReturnType<typeof parseCSSValue>[number];
+
 class stNode {
     constructor(
         public left?: string | stNode,
@@ -15,49 +17,65 @@ class stNode {
         return `${op}(${this.left}, ${this.right})`;
     }
 }
+// TODO: fix case `a|`
 
 function stExtendsParser(stExtends: string) {
     const ast = parseCSSValue(stExtends);
     const outputTree = new stNode();
     const stack: stNode[] = [outputTree];
     for (const node of ast) {
-        const currentNode = stack[stack.length - 1];
-        if (node.type === '<custom-ident>') {
+        handleNode(stack, node);
+    }
+    if (stack.length !== 1) {
+        throw new Error('unclosed parenthesis');
+    }
+    optimizeTree(outputTree);
+    return outputTree;
+}
+
+function handleNode(stack: stNode[], node: ParsedNodes) {
+    const currentNode = stack[stack.length - 1];
+    if (node.type === '<custom-ident>') {
+        if (!handleConnectedCustomIdent(stack, node)) {
             if (currentNode.op) {
                 currentNode.right = node.value;
             } else {
                 currentNode.left = node.value;
             }
-        } else if (node.type === 'literal' && node.value === '&') {
-            if (currentNode.op) {
-                pushRight(currentNode, node.value, stack);
-            } else {
-                currentNode.op = node.value;
-            }
-        } else if (node.type === 'literal' && node.value === '|') {
-            if (currentNode.op === '&') {
-                pullLeft(currentNode, node.value);
-            } else if (currentNode.op === '|') {
-                pushRight(currentNode, node.value, stack);
-            } else {
-                currentNode.op = node.value;
-            }
-        } else if (node.type === 'literal' && node.value === '(') {
-            currentNode[currentNode.op ? 'right' : 'left'] = pushStack(stack);
-        } else if (node.type === 'literal' && node.value === ')') {
-            if (currentNode.op && currentNode.right === undefined) {
-                throw new Error('Invalid ST Extends');
-            } else if (currentNode.left === undefined) {
-                throw new Error('Invalid ST Extends');
-            } else {
-                stack.pop();
-            }
-        } else if (node.type !== 'space' && node.type !== 'comment') {
-            throw new Error('Invalid ST Extends');
         }
+    } else if (node.type === 'literal' && node.value === '&') {
+        if (currentNode.op) {
+            pushRight(currentNode, node.value, stack);
+        } else {
+            if (!currentNode.left) {
+                throw new Error(`missing expression left side before ${node.value} operator`);
+            }
+            currentNode.op = node.value;
+        }
+    } else if (node.type === 'literal' && node.value === '|') {
+        if (currentNode.op === '&') {
+            pullLeft(currentNode, node.value);
+        } else if (currentNode.op === '|') {
+            pushRight(currentNode, node.value, stack);
+        } else {
+            if (!currentNode.left) {
+                throw new Error(`missing expression left side before ${node.value} operator`);
+            }
+            currentNode.op = node.value;
+        }
+    } else if (node.type === 'literal' && node.value === '(') {
+        currentNode[currentNode.op ? 'right' : 'left'] = pushStack(stack);
+    } else if (node.type === 'literal' && node.value === ')') {
+        if (currentNode.op && currentNode.right === undefined) {
+            throw new Error(`missing expression right side after ${currentNode.op} operator`);
+        } else if (currentNode.left === undefined) {
+            throw new Error('empty parenthesis');
+        } else {
+            stack.pop();
+        }
+    } else if (node.type !== 'space' && node.type !== 'comment') {
+        throw new Error(`invalid node type ${node.type} with value ${node.value}`);
     }
-    optimizeTree(outputTree);
-    return outputTree;
 }
 
 function pushStack(stack: stNode[]) {
@@ -68,7 +86,7 @@ function pushStack(stack: stNode[]) {
 
 function pullLeft(currentNode: stNode, op: '&' | '|') {
     if (!currentNode.right) {
-        throw new Error('Invalid ST Extends');
+        throw new Error(`missing value between ${currentNode.op} and ${op} operators`);
     }
     currentNode.left = currentNode.clone();
     currentNode.right = undefined;
@@ -77,7 +95,7 @@ function pullLeft(currentNode: stNode, op: '&' | '|') {
 
 function pushRight(currentNode: stNode, op: '&' | '|', stack: stNode[]) {
     if (!currentNode.right) {
-        throw new Error('Invalid ST Extends');
+        throw new Error(`missing value between ${currentNode.op} and ${op} operators`);
     }
     const newNode = new stNode(currentNode.right, undefined, op);
     currentNode.right = newNode;
@@ -113,14 +131,37 @@ function optimizeTree(stNode: string | stNode, parent?: stNode) {
     }
 }
 
+function handleConnectedCustomIdent(stack: stNode[], node: ParsedNodes) {
+    const parts = node.value.split(/(?=|)|(?=&)/gm);
+    if (parts.length > 1) {
+        let offset = 0;
+        for (const part of parts) {
+            handleNode(stack, {
+                type: part === '|' || part === '&' ? 'literal' : '<custom-ident>',
+                value: part,
+                start: node.start + offset,
+                end: node.start + offset + part.length,
+                before: '',
+                after: '',
+            });
+            offset += part.length;
+        }
+        return true;
+    }
+    return false;
+}
+
 describe('-st-extends parser', () => {
     it('parse single', () => {
         const ast = stExtendsParser('a');
         expect(ast).to.eql(new stNode('a'));
     });
+
     it('parse single |', () => {
         const ast = stExtendsParser('a | b');
         expect(ast).to.eql(new stNode('a', 'b', '|'));
+        const ast2 = stExtendsParser('a|b');
+        expect(ast2).to.eql(new stNode('a', 'b', '|'));
     });
 
     it('parse multi |', () => {
@@ -131,6 +172,8 @@ describe('-st-extends parser', () => {
     it('parse single &', () => {
         const ast = stExtendsParser('a & b');
         expect(ast).to.eql(new stNode('a', 'b', '&'));
+        const ast2 = stExtendsParser('a&b');
+        expect(ast2).to.eql(new stNode('a', 'b', '&'));
     });
 
     it('parse multi &', () => {
@@ -171,5 +214,91 @@ describe('-st-extends parser', () => {
     it('unwrap single expression', () => {
         const ast = stExtendsParser('(((a)))');
         expect(ast).to.eql(new stNode('a'));
+    });
+
+    describe('errors', () => {
+        it('unclosed parenthesis', () => {
+            expect(() => {
+                stExtendsParser('(');
+            }).to.throw('unclosed parenthesis');
+        });
+
+        it('empty parenthesis', () => {
+            expect(() => {
+                stExtendsParser('()');
+            }).to.throw('empty parenthesis');
+        });
+
+        it('missing expression right side |', () => {
+            expect(() => {
+                stExtendsParser('(a|)');
+            }).to.throw('missing expression right side after | operator');
+        });
+
+        it('missing expression right side &', () => {
+            expect(() => {
+                stExtendsParser('(a&)');
+            }).to.throw('missing expression right side after & operator');
+        });
+
+        it('missing expression left side |', () => {
+            expect(() => {
+                stExtendsParser('(|a)');
+            }).to.throw('missing expression left side before | operator');
+            expect(() => {
+                stExtendsParser('|a');
+            }).to.throw('missing expression left side before | operator');
+        });
+
+        it('missing expression left side &', () => {
+            expect(() => {
+                stExtendsParser('(&a)');
+            }).to.throw('missing expression left side before & operator');
+            expect(() => {
+                stExtendsParser('&a');
+            }).to.throw('missing expression left side before & operator');
+        });
+
+        it('consecutive | operators (no space)', () => {
+            expect(() => {
+                stExtendsParser('||');
+            }).to.throw(`invalid node type literal with value ||`);
+        });
+
+        it('consecutive & operators (no space)', () => {
+            expect(() => {
+                stExtendsParser('&&');
+            }).to.throw(`invalid node type literal with value &&`);
+        });
+
+        it('consecutive &| operators (no space)', () => {
+            expect(() => {
+                stExtendsParser('&|');
+            }).to.throw(`invalid node type literal with value &|`);
+        });
+
+        it('consecutive |& operators (no space)', () => {
+            expect(() => {
+                stExtendsParser('|&');
+            }).to.throw(`invalid node type literal with value |&`);
+        });
+
+        it('missing value with | first', () => {
+            expect(() => {
+                stExtendsParser('a | |');
+            }).to.throw(`missing value between | and | operators`);
+            expect(() => {
+                stExtendsParser('a | &');
+            }).to.throw(`missing value between | and & operators`);
+        });
+
+        it('missing value with & first', () => {
+            expect(() => {
+                stExtendsParser('a & |');
+            }).to.throw(`missing value between & and | operators`);
+            expect(() => {
+                stExtendsParser('a & &');
+            }).to.throw(`missing value between & and & operators`);
+        });
     });
 });
