97a5dac64b1b41d65eed8f4ba114698f58df921b
Tzach Bonfil <45866571+tzachbon@users.noreply.github.com>
2022-02-07 17:00:44 +0200

    feat: set specific source file path in the `dts` source-maps (#2311)

--- packages/cli/src/build-single-file.ts
+++ packages/cli/src/build-single-file.ts
@@ -175,7 +175,12 @@ export function buildSingleFile({
         // .d.ts.map
         // if not explicitly defined, assumed true with "--dts" parent scope
         if (dtsSourceMap !== false) {
-            const dtsMappingContent = generateDTSSourceMap(dtsContent, res.meta);
+            const dtsMappingContent = generateDTSSourceMap(
+                dtsContent,
+                res.meta,
+                relative(dirname(outSrcPath), dirname(outputSources ? outSrcPath : filePath))
+            );
+
             const dtsMapPath = outSrcPath + '.d.ts.map';
 
             generated.add(dtsMapPath);
