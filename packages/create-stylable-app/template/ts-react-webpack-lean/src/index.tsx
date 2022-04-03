2f10a5d7dc66f201ad485d0fc21d90beb9543e92
Tom Raviv <tomr@wix.com>
2022-04-03 05:20:38 -0700

    chore(deps): upgrade to latest stable (#2425)

--- packages/create-stylable-app/template/ts-react-webpack-lean/src/index.tsx
+++ packages/create-stylable-app/template/ts-react-webpack-lean/src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './globals.st.css';
 import { App } from './app';
 
-ReactDOM.render(
+createRoot(document.body.appendChild(document.createElement('div'))).render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>,
-    document.body.appendChild(document.createElement('div'))
+    </React.StrictMode>
 );
