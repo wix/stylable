dd1a98d99b5118adac844f9e5878b5cde238a2f7
Barak Igal <baraki@wix.com>
2022-05-18 16:33:29 +0300

    feat: implement new namespace strategy

--- packages/core/src/helpers/namespace.ts
+++ packages/core/src/helpers/namespace.ts
@@ -1,3 +1,129 @@
 export function namespace(name: string, namespace: string, delimiter = '__') {
     return namespace ? namespace + delimiter + name : name;
 }
+
+export interface PackageInfo {
+    name: string;
+    version: string;
+    dirPath: string;
+}
+
+export interface NamespaceBuilderOptions {
+    hashSalt: string;
+    prefix: string;
+    namespace: string;
+    paths: {
+        file: string;
+        origin: string;
+    };
+    packageInfo: PackageInfo;
+}
+
+export interface NamespaceBuilder {
+    (options: NamespaceBuilderOptions): {
+        namespace: string;
+        hashPart: string;
+    };
+}
+
+export interface CreateNamespaceOptions {
+    prefix?: string;
+    hashSalt?: string;
+    hashFragment?: 'full' | 'minimal' | number;
+    buildNamespace?: NamespaceBuilder;
+    getPackageInfo?: (filePath: string) => PackageInfo;
+    normalizePath: (dirPath: string, filePath: string) => string;
+    hashFn: (i: string) => string | number;
+}
+
+function defaultGetPackageInfo() {
+    return {
+        name: '',
+        version: '0.0.0',
+        dirPath: '',
+    };
+}
+
+export function defaultNamespaceBuilder({
+    prefix,
+    namespace,
+    hashSalt,
+    paths,
+    packageInfo,
+}: NamespaceBuilderOptions) {
+    return {
+        namespace: prefix + namespace,
+        hashPart: hashSalt + packageInfo.name + '@' + packageInfo.version + '/' + paths.origin,
+    };
+}
+
+// function defaultHash(input: string) {
+//     //Node12 compatibility - we might want to use base64Url instead
+//     return createHash('sha256').update(input).digest('hex');
+// }
+
+// function normPath(packageRoot: string, stylesheetPath: string) {
+//     return relative(packageRoot, stylesheetPath).replace(/\\/g, '/');
+// }
+
+export function createNamespaceStrategy(options: CreateNamespaceOptions) {
+    const {
+        prefix = '',
+        hashSalt = '',
+        hashFragment = 'minimal',
+        buildNamespace = defaultNamespaceBuilder,
+        getPackageInfo = defaultGetPackageInfo,
+        hashFn,
+        normalizePath,
+    } = options;
+
+    const usedNamespaces = new Map<string, string>();
+
+    return (
+        namespace: string,
+        stylesheetPath: string,
+        stylesheetOriginPath: string = stylesheetPath
+    ) => {
+        const packageInfo = getPackageInfo(stylesheetPath);
+
+        const results = buildNamespace({
+            prefix,
+            hashSalt,
+            namespace,
+            paths: {
+                file: packageInfo.dirPath
+                    ? normalizePath(packageInfo.dirPath, stylesheetPath)
+                    : stylesheetPath,
+                origin: packageInfo.dirPath
+                    ? normalizePath(packageInfo.dirPath, stylesheetOriginPath)
+                    : stylesheetOriginPath,
+            },
+            packageInfo,
+        });
+
+        const { namespace: resultNs, hashPart } = results;
+
+        const hashStr = hashFn(hashPart).toString();
+        let i =
+            typeof hashFragment === 'number'
+                ? hashFragment
+                : hashFragment === 'full'
+                ? hashStr.length
+                : 0;
+
+        while (i <= hashStr.length) {
+            const hashSlice = hashStr.slice(0, i);
+            const ns = resultNs + (hashSlice ? '-' + hashSlice : '');
+            const used = usedNamespaces.get(ns);
+            if (!used) {
+                usedNamespaces.set(ns, stylesheetPath);
+                return ns;
+            }
+            if (used === stylesheetPath) {
+                return ns;
+            }
+            i++;
+        }
+        throw new Error('Could not create namespace for ' + stylesheetPath);
+    };
+}
---
+++ packages/core/test/helpers/namespace.spec.ts
@@ -0,0 +1,154 @@
+import { expect } from 'chai';
+import {
+    type NamespaceBuilderOptions,
+    createNamespaceStrategy,
+    defaultNamespaceBuilder,
+} from '@stylable/core/dist/helpers/namespace';
+
+describe('createNamespaceStrategy', () => {
+    it('should return smallest namespace for the same file', () => {
+        const resolveNamespace = createNamespaceStrategy({
+            hashFn: () => '1',
+            getPackageInfo: () => ({ name: 'package', version: '0.0.0', dirPath: '/package' }),
+            normalizePath: (dirPath, filePath) => filePath.replace(dirPath, ''),
+        });
+
+        expect(resolveNamespace('x', '/package/x.st.css')).to.equal('x');
+        expect(resolveNamespace('x', '/package/x.st.css')).to.equal('x');
+    });
+    it('should return smallest namespace for the same file with prefix', () => {
+        const resolveNamespace = createNamespaceStrategy({
+            prefix: 'test-',
+            hashFn: () => '1',
+            getPackageInfo: () => ({ name: 'package', version: '0.0.0', dirPath: '/package' }),
+            normalizePath: (dirPath, filePath) => filePath.replace(dirPath, ''),
+        });
+
+        expect(resolveNamespace('x', '/package/x.st.css')).to.equal('test-x');
+        expect(resolveNamespace('x', '/package/x.st.css')).to.equal('test-x');
+    });
+    it('should salt the hash', () => {
+        let usedInput: string | undefined;
+        const resolveNamespace = createNamespaceStrategy({
+            hashSalt: '__SALT__',
+            hashFn: (input) => {
+                usedInput = input;
+                return '1';
+            },
+            getPackageInfo: () => ({ name: 'package', version: '0.0.0', dirPath: '/package' }),
+            normalizePath: (dirPath, filePath) => filePath.replace(dirPath, ''),
+        });
+
+        expect(resolveNamespace('x', '/package/x.st.css')).to.equal('x');
+        expect(usedInput).to.equal('__SALT__package@0.0.0//x.st.css');
+    });
+    it('should call buildNamespace hook', () => {
+        let usedOptions: NamespaceBuilderOptions | undefined;
+        const resolveNamespace = createNamespaceStrategy({
+            hashFn: () => '1',
+            getPackageInfo: () => ({ name: 'package', version: '0.0.0', dirPath: '/package' }),
+            normalizePath: (dirPath, filePath) => filePath.replace(dirPath, ''),
+            buildNamespace(options) {
+                usedOptions = options;
+                return defaultNamespaceBuilder(options);
+            },
+        });
+
+        expect(resolveNamespace('x', '/package/x.st.css')).to.equal('x');
+        expect(usedOptions).to.eql({
+            prefix: '',
+            hashSalt: '',
+            namespace: 'x',
+            paths: {
+                file: '/x.st.css',
+                origin: '/x.st.css',
+            },
+            packageInfo: {
+                name: 'package',
+                version: '0.0.0',
+                dirPath: '/package',
+            },
+        });
+    });
+    it('should add a small part of the hash for same namespace from different file', () => {
+        let nextHash = 0;
+        const resolveNamespace = createNamespaceStrategy({
+            hashFn: () => nextHash++,
+            getPackageInfo: () => ({ name: 'package', version: '0.0.0', dirPath: '/package' }),
+            normalizePath: (dirPath, filePath) => filePath.replace(dirPath, ''),
+        });
+
+        expect(resolveNamespace('x', '/package/x.st.css')).to.equal('x'); // hash 0
+        expect(resolveNamespace('x', '/package/x1.st.css')).to.equal('x-1'); // hash 1
+        expect(resolveNamespace('x', '/package/x2.st.css')).to.equal('x-2'); // hash 2
+    });
+    it('should throw when no unique namespace can be generated and hash slice size is larger then hash length', () => {
+        const resolveNamespace = createNamespaceStrategy({
+            hashFn: () => '1',
+            getPackageInfo: () => ({ name: 'package', version: '0.0.0', dirPath: '/package' }),
+            normalizePath: (dirPath, filePath) => filePath.replace(dirPath, ''),
+        });
+        expect(resolveNamespace('x', '/package/x.st.css')).to.equal('x');
+        expect(resolveNamespace('x', '/package/x1.st.css')).to.equal('x-1');
+        expect(() => {
+            resolveNamespace('x', '/package/x2.st.css');
+        }).to.throw('Could not create namespace for /package/x2.st.css');
+    });
+    it('should use minimum hash slice size', () => {
+        const resolveNamespace = createNamespaceStrategy({
+            hashFragment: 4, // min size hash slice
+            hashFn: () => '12345',
+            getPackageInfo: () => ({ name: 'package', version: '0.0.0', dirPath: '/package' }),
+            normalizePath: (dirPath, filePath) => filePath.replace(dirPath, ''),
+        });
+
+        expect(resolveNamespace('x', '/package/x.st.css')).to.equal('x-1234');
+        expect(resolveNamespace('x', '/package/x1.st.css')).to.equal('x-12345');
+    });
+
+    it('should use full hash', () => {
+        const resolveNamespace = createNamespaceStrategy({
+            hashFragment: 'full', // min size hash slice
+            hashFn: () => '12345',
+            getPackageInfo: () => ({ name: 'package', version: '0.0.0', dirPath: '/package' }),
+            normalizePath: (dirPath, filePath) => filePath.replace(dirPath, ''),
+        });
+
+        expect(resolveNamespace('x', '/package/x.st.css')).to.equal('x-12345');
+    });
+
+    it('should use stylesheet path when dirPath from getPackageInfo does not exists', () => {
+        let usedInput: string | undefined;
+        const resolveNamespace = createNamespaceStrategy({
+            hashFn: (input) => {
+                usedInput = input;
+                return '12345';
+            },
+            getPackageInfo: () => ({ name: 'package', version: '0.0.0', dirPath: '' }),
+            normalizePath: () => {
+                throw new Error('Should not be called');
+            },
+        });
+
+        expect(resolveNamespace('x', '/package/x.st.css')).to.equal('x');
+        expect(usedInput).to.equal('package@0.0.0//package/x.st.css');
+    });
+
+    it('should use normalizePath when dirPath from getPackageInfo exists', () => {
+        let usedDirPath: string | undefined;
+        let usedFilePath: string | undefined;
+        const resolveNamespace = createNamespaceStrategy({
+            hashFn: () => '12345',
+            getPackageInfo: () => ({ name: 'package', version: '0.0.0', dirPath: '/package' }),
+            normalizePath: (dirPath, filePath) => {
+                usedDirPath = dirPath;
+                usedFilePath = filePath;
+                return filePath.replace(dirPath, '');
+            },
+        });
+
+        expect(resolveNamespace('x', '/package/x.st.css')).to.equal('x');
+        expect(usedDirPath).to.equal('/package');
+        expect(usedFilePath).to.equal('/package/x.st.css');
+    });
+});
