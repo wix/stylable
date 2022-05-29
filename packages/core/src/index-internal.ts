596803e2ff55609d4d3e500aa2696c00d852cab3
Tom Raviv <tomr@wix.com>
2022-05-29 04:44:48 -0700

    fix(language-service): refactor document sanitaizing (#2531)

--- packages/core/src/index-internal.ts
+++ packages/core/src/index-internal.ts
@@ -1,4 +1,5 @@
 export { safeParse } from './parser';
+export { getAstNodeAt } from './helpers/ast';
 export { process } from './stylable-processor';
 export { StylableTransformer } from './stylable-transformer';
 export type { MappedStates } from './features';
