ef1633933a32d2d68524ce7c64092377ebbfc641
Ido Rosenthal <idoros@users.noreply.github.com>
2022-06-09 14:22:37 +0300

    feat(core): import analyze API (#2549)

--- packages/core/src/stylable.ts
+++ packages/core/src/stylable.ts
@@ -14,7 +14,7 @@ import {
 } from './stylable-transformer';
 import type { IStylableOptimizer, ModuleResolver } from './types';
 import { createDefaultResolver } from './module-resolver';
-import { STVar, STMixin, CSSCustomProperty } from './features';
+import { STImport, STVar, STMixin, CSSCustomProperty } from './features';
 import { Dependency, visitMetaCSSDependencies } from './visit-meta-css-dependencies';
 import * as postcss from 'postcss';
 
@@ -48,6 +48,7 @@ export type CreateProcessorOptions = Pick<StylableConfig, 'resolveNamespace'>;
 export class Stylable {
     public fileProcessor: FileProcessor<StylableMeta>;
     public resolver: StylableResolver;
+    public stModule = new STImport.StylablePublicApi(this);
     public stVar = new STVar.StylablePublicApi(this);
     public stMixin = new STMixin.StylablePublicApi(this);
     //
