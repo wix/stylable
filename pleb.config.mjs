38b2afbf3205605d195acaf0e15b96f208590e3f
Barak Igal <baraki@wix.com>
2022-07-16 23:57:46 +0300

    chore: remove jsdom

--- pleb.config.mjs
+++ pleb.config.mjs
@@ -1,6 +1,5 @@
 export default {
     pinnedPackages: [
-        { name: 'jsdom', reason: 'drops support for node v12' },
         { name: 'source-map-loader', reason: 'drops support for node v12' },
         { name: '@wixc3/resolve-directory-context', reason: 'drops support for node v12' },
         { name: 'vscode-languageserver', reason: 'drops support for node v12' },
