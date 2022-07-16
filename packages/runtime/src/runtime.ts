8bb22fdf44992a3cca94bde17fe830383ef1c5e3
Barak Igal <baraki@wix.com>
2022-07-16 23:29:07 +0300

    refactor!: remove old runtime. export single pure runtime

--- packages/runtime/src/runtime.ts
+++ packages/runtime/src/runtime.ts
@@ -1,89 +1,105 @@
 /* eslint-disable no-var */
-import type { Host, StateMap, StateValue } from './types';
+import type { StateMap, StateValue } from './types';
 
-export function stylesheet(host?: Host) {
-    host = host || {};
-    var stateMiddleDelimiter = '-';
-    var booleanStateDelimiter = '--';
-    var stateWithParamDelimiter = '---';
+export type {
+    ClassesMap,
+    RuntimeStylesheet,
+    STFunction,
+    StateMap,
+    StateValue,
+    StylableExports,
+    RuntimeStVar,
+} from './types';
 
-    function createBooleanStateClassName(namespace: string, stateName: string) {
-        return namespace + booleanStateDelimiter + stateName;
+function createStateClass(namespace: string, stateName: string, stateValue: StateValue): string {
+    if (
+        stateValue === false ||
+        stateValue === undefined ||
+        stateValue === null ||
+        stateValue !== stateValue // check NaN
+    ) {
+        return '';
     }
 
-    function createStateWithParamClassName(namespace: string, stateName: string, param: string) {
-        return (
-            namespace +
-            stateWithParamDelimiter +
-            stateName +
-            stateMiddleDelimiter +
-            param.length +
-            stateMiddleDelimiter +
-            param.replace(/\s/gm, '_')
-        );
+    if (stateValue === true) {
+        // boolean state
+        return namespace + '--' + stateName;
     }
 
-    function createStateClass(
-        namespace: string,
-        stateName: string,
-        stateValue: StateValue
-    ): string {
-        if (
-            stateValue === false ||
-            stateValue === undefined ||
-            stateValue === null ||
-            stateValue !== stateValue // check NaN
-        ) {
-            return '';
-        }
+    var param = stateValue.toString();
 
-        if (stateValue === true) {
-            // boolean state
-            return createBooleanStateClassName(namespace, stateName);
-        }
+    return namespace + '---' + stateName + '-' + param.length + '-' + param.replace(/\s/gm, '_');
+}
 
-        var valueAsString = stateValue.toString();
+export function classesRuntime(namespace: string): string {
+    var classNames = [];
 
-        return createStateWithParamClassName(namespace, stateName, valueAsString);
-    }
+    for (var i = 1; i < arguments.length; i++) {
+        // eslint-disable-next-line prefer-rest-params
+        var item = arguments[i];
 
-    function style(namespace: string) {
-        var classNames = [];
-
-        for (var i = 1; i < arguments.length; i++) {
-            // eslint-disable-next-line prefer-rest-params
-            var item = arguments[i];
-
-            if (item) {
-                if (typeof item === 'string') {
-                    classNames[classNames.length] = item;
-                } else if (i === 2) {
-                    for (var stateName in item) {
-                        var stateValue = item[stateName];
-                        var stateClass = createStateClass(namespace, stateName, stateValue);
-                        if (stateClass) {
-                            classNames[classNames.length] = stateClass;
-                        }
+        if (item) {
+            if (typeof item === 'string') {
+                classNames[classNames.length] = item;
+            } else if (i === 2) {
+                for (var stateName in item) {
+                    var stateValue = item[stateName];
+                    var stateClass = createStateClass(namespace, stateName, stateValue);
+                    if (stateClass) {
+                        classNames[classNames.length] = stateClass;
                     }
                 }
             }
         }
-        return classNames.join(' ');
     }
+    return classNames.join(' ');
+}
 
-    function cssStates(namespace: string, stateMapping?: StateMap | null): string {
-        var classNames = [];
-        for (var stateName in stateMapping) {
-            var stateValue = stateMapping[stateName];
-            var stateClass = createStateClass(namespace, stateName, stateValue);
-            if (stateClass) {
-                classNames.push(stateClass);
+export function statesRuntime(namespace: string, stateMapping?: StateMap | null): string {
+    var classNames = [];
+    for (var stateName in stateMapping) {
+        var stateValue = stateMapping[stateName];
+        var stateClass = createStateClass(namespace, stateName, stateValue);
+        if (stateClass) {
+            classNames.push(stateClass);
+        }
+    }
+    return classNames.join(' ');
+}
+
+export function injectCSS(namespace: string, css: string, depth: number, runtimeId: string): void {
+    if (typeof document === 'undefined') {
+        return;
+    }
+    var d = document;
+    var head = d.head;
+    var style = d.createElement('style');
+    style.setAttribute('st_id', namespace);
+    style.setAttribute('st_depth', depth as unknown as string);
+    style.setAttribute('st_runtime', runtimeId);
+    style.textContent = css;
+    var loadedStyleElements = head.querySelectorAll<HTMLStyleElement>(
+        'style[st_runtime="' + runtimeId + '"]'
+    );
+    var inserted = false;
+    for (var i = 0; i < loadedStyleElements.length; i++) {
+        var styleElement = loadedStyleElements[i];
+        var stId = styleElement.getAttribute('st_id');
+        var stDepth = Number(styleElement.getAttribute('st_depth'));
+        if (stId === namespace) {
+            if (stDepth === depth) {
+                head.replaceChild(style, styleElement);
+                return;
+            } else {
+                styleElement.parentElement!.removeChild(styleElement);
             }
         }
-        return classNames.join(' ');
+        if (!inserted && depth < stDepth) {
+            head.insertBefore(style, styleElement);
+            inserted = true;
+        }
     }
-
-    host.sts = style;
-    host.stc = cssStates;
-    return host as Required<Pick<Host, 'stc' | 'sts'>>;
+    if (!inserted) {
+        head.append(style);
+    }
 }
---
+++ packages/runtime/test/unit/micro-document.ts
@@ -0,0 +1,79 @@
+export interface MicroElementList {
+    length: number;
+    [key: number]: MicroElement;
+}
+
+export class MicroElement {
+    constructor(private tagName: string) {}
+    private _attrs: Record<string, string> = {};
+    private _children = new Array<MicroElement>();
+    parentElement: MicroElement | null = null;
+    textContent = '';
+    get children(): MicroElementList {
+        return this._children;
+    }
+    setAttribute(name: string, value: string): void {
+        this._attrs[name] = String(value);
+    }
+
+    getAttribute(name: string): string | null {
+        return this._attrs[name] || null;
+    }
+
+    replaceChild(newChild: MicroElement, oldChild: MicroElement): MicroElement {
+        const index = this._children.indexOf(oldChild);
+        if (index >= 0) {
+            this._children.splice(index, 1, newChild);
+            this.removeChild(oldChild);
+        }
+        return oldChild;
+    }
+
+    insertBefore(newChild: MicroElement, refChild?: MicroElement): void {
+        newChild.remove();
+        const index = refChild ? this._children.indexOf(refChild) : -1;
+        if (index >= 0) {
+            this._children.splice(index, 0, newChild);
+        } else {
+            this._children.push(newChild);
+        }
+        newChild.parentElement = this;
+    }
+
+    removeChild(oldChild: MicroElement): void {
+        const index = this._children.indexOf(oldChild);
+        if (index >= 0) {
+            this._children.splice(index, 1);
+            oldChild.parentElement = null;
+        }
+    }
+
+    append(newChild: MicroElement): void {
+        this.insertBefore(newChild);
+    }
+    remove(): void {
+        this.parentElement?.removeChild(this);
+    }
+
+    querySelectorAll(selector: string): MicroElementList {
+        // only one selector supported
+        const m = selector.match(/style\[st_runtime="(.*?)"\]/);
+        if (!m) {
+            throw new Error('Not implemented ' + selector);
+        }
+        return this._children.filter(
+            (c) => c.tagName === 'style' && c.getAttribute('st_runtime') === m[1]
+        );
+    }
+
+    stylableIds() {
+        return this._children.map((c) => c.getAttribute('st_id')).join(', ');
+    }
+}
+
+export class MicroDocument {
+    head = new MicroElement('head');
+    createElement(tagName: string): MicroElement {
+        return new MicroElement(tagName);
+    }
+}
