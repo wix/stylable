438b3bbd327491a6dc0dea7ee167dc00b40bfc16
Ido Rosenthal <idoros@gmail.com>
2022-07-21 11:02:45 +0300

    test: move namespace tests to feature spec

--- packages/core/test/stylable-processor.spec.ts
+++ packages/core/test/stylable-processor.spec.ts
@@ -6,12 +6,10 @@ import {
     knownPseudoClassesWithNestedSelectors,
     processorDiagnostics,
 } from '@stylable/core/dist/index-internal';
-import { STNamespace } from '@stylable/core/dist/features';
 
 chai.use(flatMatch);
 
 const processorStringDiagnostics = diagnosticBankReportToStrings(processorDiagnostics);
-const stNamespaceDiagnostics = diagnosticBankReportToStrings(STNamespace.diagnostics);
 
 describe('Stylable postcss process', () => {
     it('report if missing filename', () => {
@@ -20,24 +18,6 @@ describe('Stylable postcss process', () => {
         expect(diagnostics.reports[0]).to.include({
             severity: 'error',
             message: 'missing source filename',
-        });
-    });
-
-    it('report on invalid namespace', () => {
-        const { diagnostics } = processSource(`@namespace App;`, { from: '/path/to/source' });
-
-        expect(diagnostics.reports[0]).to.include({
-            severity: 'error',
-            message: stNamespaceDiagnostics.INVALID_NAMESPACE_DEF(),
-        });
-    });
-
-    it('warn on empty-ish namespace', () => {
-        const { diagnostics } = processSource(`@namespace '   ';`, { from: '/path/to/source' });
-
-        expect(diagnostics.reports[0]).to.include({
-            severity: 'error',
-            message: stNamespaceDiagnostics.EMPTY_NAMESPACE_DEF(),
         });
     });
 
@@ -56,48 +36,6 @@ describe('Stylable postcss process', () => {
             severity: 'error',
             message: processorStringDiagnostics.INVALID_NESTING('.y', '.x'),
         });
-    });
-
-    it('collect namespace', () => {
-        const from = resolve('/path/to/style.css');
-        const result = processSource(
-            `
-            @namespace "name";
-            @namespace 'anther-name';
-        `,
-            { from }
-        );
-
-        expect(result.namespace).to.equal(processNamespace('anther-name', from));
-    });
-
-    it('resolve namespace hook', () => {
-        const from = resolve('/path/to/style.css');
-        const result = processSource(
-            `
-            @namespace "name";
-        `,
-            { from },
-            (s) => 'Test-' + s
-        );
-
-        expect(result.namespace).to.equal('Test-name');
-    });
-
-    it('use filename as default namespace prefix', () => {
-        const from = resolve('/path/to/style.st.css');
-        const distFrom = resolve('/dist/path/to/style.st.css');
-
-        const result = processSource(
-            `
-            /* st-namespace-reference="../../../path/to/style.st.css" */\n
-        `,
-            { from: distFrom }
-        );
-
-        // assure namespace generated with st-namespace-reference
-        // is identical between source and dist with the relative correction
-        expect(result.namespace).to.eql(processNamespace('style', from));
     });
 
     it('use filename as default namespace prefix (empty)', () => {
