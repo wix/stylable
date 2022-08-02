b69ea8cc076355ae0488da1c7614777d27bab382
Ido Rosenthal <idoros@gmail.com>
2022-08-02 11:59:01 +0300

    feat(language-service): support `@st-namespace`
    - replace `@namespace` with `@st-namespace`
    - suppress `unknown at-rule` diagnostic

--- packages/language-service/src/lib/completion-providers.ts
+++ packages/language-service/src/lib/completion-providers.ts
@@ -290,7 +290,7 @@ export const RulesetInternalDirectivesProvider: CompletionProvider & {
 };
 
 // Only top level
-// @namespace may not repeat
+// @st-namespace may not repeat
 export const TopLevelDirectiveProvider: CompletionProvider = {
     provide({
         parentSelector,
