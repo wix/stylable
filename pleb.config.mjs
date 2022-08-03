db2ad1cc8669f5f890b2d663804a7f14489f15a2
Tom Raviv <tomr@wix.com>
2022-08-03 13:26:30 +0300

    chore(deps): upgrade to latest stable

--- pleb.config.mjs
+++ pleb.config.mjs
@@ -1,9 +1,14 @@
 export default {
-    pinnedPackages: [
-        { name: 'jsdom', reason: 'drops support for node v12' },
-        { name: 'source-map-loader', reason: 'drops support for node v12' },
-        { name: '@wixc3/resolve-directory-context', reason: 'drops support for node v12' },
-        { name: 'vscode-languageserver', reason: 'drops support for node v12' },
-        { name: 'vscode-css-languageservice', reason: 'drops support for node v12' },
-    ],
+  pinnedPackages: [
+    { name: '@file-services/memory', reason: 'drops support for node v12' },
+    { name: '@file-services/node', reason: 'drops support for node v12' },
+    { name: '@file-services/types', reason: 'drops support for node v12' },
+    { name: '@file-services/typescript', reason: 'drops support for node v12' },
+    { name: '@types/jsdom', reason: 'drops support for node v12' },
+    { name: '@wixc3/resolve-directory-context', reason: 'drops support for node v12' },
+    { name: 'jsdom', reason: 'drops support for node v12' },
+    { name: 'source-map-loader', reason: 'drops support for node v12' },
+    { name: 'vscode-languageserver', reason: 'drops support for node v12' },
+    { name: 'vscode-css-languageservice', reason: 'drops support for node v12' },
+  ],
 };
