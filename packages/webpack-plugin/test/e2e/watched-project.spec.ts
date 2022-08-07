61cae32c8293a5090b439cfa485fe26da7facfb6
Ido Rosenthal <idoros@gmail.com>
2022-08-07 12:26:56 +0300

    run out-of-memory tests multiple times with logs

--- packages/webpack-plugin/test/e2e/watched-project.spec.ts
+++ packages/webpack-plugin/test/e2e/watched-project.spec.ts
@@ -9,73 +9,76 @@ const project = 'watched-project';
 const projectDir = dirname(
     require.resolve(`@stylable/webpack-plugin/test/e2e/projects/${project}/webpack.config`)
 );
-
-describe(`(${project})`, () => {
-    const projectRunner = StylableProjectRunner.mochaSetup(
-        {
-            projectDir,
-            launchOptions: {
-                // headless: false
+let i = 50;
+while (--i > 0) {
+    describe(`(${project})`, () => {
+        const projectRunner = StylableProjectRunner.mochaSetup(
+            {
+                projectDir,
+                launchOptions: {
+                    // headless: false
+                },
+                watchMode: true,
+                useTempDir: true,
+                log: true,
             },
-            watchMode: true,
-            useTempDir: true,
-        },
-        before,
-        afterEach,
-        after
-    );
-    it('renders css', async () => {
-        const { page } = await projectRunner.openInBrowser();
-        const styleElements = await page.evaluate(browserFunctions.getStyleElementsMetadata, {
-            includeCSSContent: true,
+            before,
+            afterEach,
+            after
+        );
+        it('renders css', async () => {
+            const { page } = await projectRunner.openInBrowser();
+            const styleElements = await page.evaluate(browserFunctions.getStyleElementsMetadata, {
+                includeCSSContent: true,
+            });
+
+            expect(styleElements[0]).to.include({
+                id: './src/index.st.css',
+                depth: '3',
+            });
+
+            expect(styleElements[0].css!.replace(/\s\s*/gm, ' ').trim()).to.match(
+                /\.index\d+__root \{ color: red; font-size: 3em; z-index: 1; \}/
+            );
+
+            await projectRunner.actAndWaitForRecompile(
+                'invalidate dependency',
+                () => {
+                    return writeFile(
+                        join(projectRunner.testDir, 'src', 'mixin-b.st.css'),
+                        '.b{ color: green; }'
+                    );
+                },
+                async () => {
+                    const { page } = await projectRunner.openInBrowser();
+                    const styleElements = await page.evaluate(
+                        browserFunctions.getStyleElementsMetadata,
+                        {
+                            includeCSSContent: true,
+                        }
+                    );
+                    expect(styleElements[0].css!.replace(/\s\s*/gm, ' ').trim()).to.match(
+                        /\.index\d+__root \{ color: green; font-size: 3em; z-index: 1; \}/
+                    );
+                }
+            );
         });
 
-        expect(styleElements[0]).to.include({
-            id: './src/index.st.css',
-            depth: '3',
+        it('allow stylable imports with missing files', async () => {
+            await projectRunner.actAndWaitForRecompile(
+                'rename files with invalid dependencies',
+                () => {
+                    return rename(
+                        join(projectRunner.testDir, 'src', 'index.st.css'),
+                        join(projectRunner.testDir, 'src', 'xxx.st.css')
+                    );
+                },
+                () => {
+                    // if we got here we finished to recompile with the missing file.
+                    // when this test is broken the compiler had en error and exit the process.
+                    expect('finish recompile');
+                }
+            );
         });
-
-        expect(styleElements[0].css!.replace(/\s\s*/gm, ' ').trim()).to.match(
-            /\.index\d+__root \{ color: red; font-size: 3em; z-index: 1; \}/
-        );
-
-        await projectRunner.actAndWaitForRecompile(
-            'invalidate dependency',
-            () => {
-                return writeFile(
-                    join(projectRunner.testDir, 'src', 'mixin-b.st.css'),
-                    '.b{ color: green; }'
-                );
-            },
-            async () => {
-                const { page } = await projectRunner.openInBrowser();
-                const styleElements = await page.evaluate(
-                    browserFunctions.getStyleElementsMetadata,
-                    {
-                        includeCSSContent: true,
-                    }
-                );
-                expect(styleElements[0].css!.replace(/\s\s*/gm, ' ').trim()).to.match(
-                    /\.index\d+__root \{ color: green; font-size: 3em; z-index: 1; \}/
-                );
-            }
-        );
     });
-
-    it('allow stylable imports with missing files', async () => {
-        await projectRunner.actAndWaitForRecompile(
-            'rename files with invalid dependencies',
-            () => {
-                return rename(
-                    join(projectRunner.testDir, 'src', 'index.st.css'),
-                    join(projectRunner.testDir, 'src', 'xxx.st.css')
-                );
-            },
-            () => {
-                // if we got here we finished to recompile with the missing file.
-                // when this test is broken the compiler had en error and exit the process.
-                expect('finish recompile');
-            }
-        );
-    });
-});
+}
