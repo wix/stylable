68d592c2877b52c9abf3a65ae970b638312541e0
Tom Raviv <tomr@wix.com>
2022-11-07 16:53:21 +0200

    chore: nest node test fixtures in directories

--- packages/node/test/require-hook.spec.ts
+++ packages/node/test/require-hook.spec.ts
@@ -4,7 +4,9 @@ import { murmurhash3_32_gc } from '@stylable/core/dist/index-internal';
 import { dirname, join } from 'path';
 import { attachHook } from '@stylable/node';
 
-const fixturesPath = dirname(require.resolve('@stylable/node/test/fixtures/package.json'));
+const fixturesPath = dirname(
+    require.resolve('@stylable/node/test/fixtures/base-project/package.json')
+);
 
 describe('require hook', () => {
     afterEach(() => {
