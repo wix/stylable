3cd048b7f18b014633b64bb4b0f8a8e45b5c821e
Avi Vahl <avi.vahl@wix.com>
2023-03-22 18:13:06 +0200

    test(node): use link instead of npm in loader test also print stderr when spawn fails

--- packages/node/test/loader.spec.ts
+++ packages/node/test/loader.spec.ts
@@ -1,29 +1,31 @@
-import { spawnSync, execSync } from 'child_process';
+import { spawnSync } from 'child_process';
 import { join, dirname } from 'path';
+import { symlinkSync } from 'fs';
 import { createTempDirectorySync } from '@stylable/core-test-kit';
 import { expect } from 'chai';
 
-function runTest(fixturePath: string) {
-    const fixtureDirPath = dirname(fixturePath);
-    execSync('npm install', { cwd: fixtureDirPath });
-    return spawnSync('node', ['--experimental-loader', '@stylable/node/loader.mjs', fixturePath], {
-        encoding: 'utf8',
-        cwd: fixtureDirPath,
-    });
-}
-
-const stylableRuntimeDepPath =
-    '"file:' + JSON.stringify(join(__dirname, '../../../runtime')).substring(1);
-const stylableNodeDepPath =
-    '"file:' + JSON.stringify(join(__dirname, '../../../node')).substring(1);
-
+const rootRepoPath = dirname(require.resolve('../../../../package.json'));
+const describeIf = (condition: boolean) => (condition ? describe : describe.skip);
 // ToDo(major): remove conditional once node 14 support is dropped
 const nodeMajorVersion = Number(process.versions.node.split('.')[0]);
-(nodeMajorVersion > 14 ? describe : describe.skip)('node loader', () => {
+
+const spawnNodeWithLoader = (fixtureFilePath: string) =>
+    spawnSync('node', ['--experimental-loader', '@stylable/node/loader.mjs', fixtureFilePath], {
+        encoding: 'utf8',
+        cwd: dirname(fixtureFilePath),
+    });
+
+describeIf(nodeMajorVersion > 14)('node loader', () => {
     let tempDir: ReturnType<typeof createTempDirectorySync>;
     beforeEach('crate temp dir', () => {
         tempDir = createTempDirectorySync('st-node-loader-');
+        symlinkSync(
+            join(rootRepoPath, 'node_modules'),
+            join(tempDir.path, 'node_modules'),
+            'junction'
+        );
     });
+
     afterEach('remove temp dir', () => {
         tempDir.remove();
     });
@@ -44,20 +46,18 @@ const nodeMajorVersion = Number(process.versions.node.split('.')[0]);
                 {
                     "name": "test",
                     "version": "0.0.1",
-                    "type": "module",
-                    "dependencies": {
-                        "@stylable/runtime": ${stylableRuntimeDepPath},
-                        "@stylable/node": ${stylableNodeDepPath}
-                    }
+                    "type": "module"
                 }
             `,
         });
         const fixturePath = join(tempDir.path, 'index.js');
 
-        const result = runTest(fixturePath);
+        const result = spawnNodeWithLoader(fixturePath);
 
+        expect(result.status, result.stderr).to.equal(0);
         expect(result.stdout).to.eql(`{ classes: [ 'root', 'part' ] }\n`);
     });
+
     it('should use stylable.config (esm project)', () => {
         tempDir.setContent({
             'index.st.css': `
@@ -81,20 +81,20 @@ const nodeMajorVersion = Number(process.versions.node.split('.')[0]);
                 {
                     "name": "test",
                     "version": "0.0.1",
-                    "type": "module",
-                    "dependencies": {
-                        "@stylable/runtime": ${stylableRuntimeDepPath},
-                        "@stylable/node": ${stylableNodeDepPath}
-                    }
+                    "type": "module"
                 }
             `,
         });
         const fixturePath = join(tempDir.path, 'index.js');
 
-        const result = runTest(fixturePath);
+        const result = spawnNodeWithLoader(fixturePath);
 
-        expect(result.stdout).to.eql(`{ root: 'x-index__root', part: 'x-index__part' }\n`);
+        expect(result.status, result.stderr).to.equal(0);
+        expect(result.stdout, result.stderr).to.eql(
+            `{ root: 'x-index__root', part: 'x-index__part' }\n`
+        );
     });
+
     it('should use stylable.config (cjs project)', () => {
         tempDir.setContent({
             'index.st.css': `
@@ -119,18 +119,17 @@ const nodeMajorVersion = Number(process.versions.node.split('.')[0]);
             'package.json': `
                 {
                     "name": "test",
-                    "version": "0.0.1",
-                    "dependencies": {
-                        "@stylable/runtime": ${stylableRuntimeDepPath},
-                        "@stylable/node": ${stylableNodeDepPath}
-                    }
+                    "version": "0.0.1"
                 }
             `,
         });
         const fixturePath = join(tempDir.path, 'index.mjs');
 
-        const result = runTest(fixturePath);
+        const result = spawnNodeWithLoader(fixturePath);
 
-        expect(result.stdout).to.eql(`{ root: 'x-index__root', part: 'x-index__part' }\n`);
+        expect(result.status, result.stderr).to.equal(0);
+        expect(result.stdout, result.stderr).to.eql(
+            `{ root: 'x-index__root', part: 'x-index__part' }\n`
+        );
     });
 });
