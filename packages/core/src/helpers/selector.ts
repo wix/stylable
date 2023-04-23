882a0406509b4204667a715b637af4c78f5a6f3b
Ido Rosenthal <idoros@gmail.com>
2023-04-23 17:50:07 +0300

    refactor: moved default arg to function signature

--- packages/core/src/helpers/selector.ts
+++ packages/core/src/helpers/selector.ts
@@ -167,6 +167,7 @@ export function isCompRoot(name: string) {
     return name.charAt(0).match(/[A-Z]/);
 }
 
+const isNestedNode = (node: SelectorNode) => node.type === 'nesting';
 /**
  * combine 2 selector lists.
  * - add each scoping selector at the begging of each nested selector
@@ -176,7 +177,7 @@ export function scopeNestedSelector(
     scopeSelectorAst: ImmutableSelectorList,
     nestedSelectorAst: ImmutableSelectorList,
     rootScopeLevel = false,
-    isAnchor?: (node: SelectorNode) => boolean
+    isAnchor: (node: SelectorNode) => boolean = isNestedNode
 ): { selector: string; ast: SelectorList } {
     const resultSelectors: SelectorList = [];
     nestedSelectorAst.forEach((targetAst) => {
@@ -204,7 +205,7 @@ export function scopeNestedSelector(
                 : false;
             let nestedMixRoot = false;
             walkSelector(outputAst, (node, i, nodes) => {
-                if (isAnchor ? isAnchor(node) : node.type === 'nesting') {
+                if (isAnchor(node)) {
                     nestedMixRoot = true;
                     nodes.splice(i, 1, {
                         type: `selector`,
