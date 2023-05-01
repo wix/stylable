6dd7e9763c114fad08e8323b06df4a17d9b3197e
Barak Igal <baraki@wix.com>
2023-05-01 17:45:38 +0300

    add rebuild test

---
+++ packages/esbuild/test/e2e/rebuild/rebuild.spec.ts
@@ -0,0 +1,24 @@
+import { expect } from 'chai';
+import { ESBuildTestKit } from '../esbuild-testkit';
+import { sleep } from 'promise-assist';
+
+describe('Stylable ESBuild plugin rebuild on change', () => {
+    const tk = new ESBuildTestKit();
+
+    afterEach(() => tk.dispose());
+
+    it('should pick up rebuild', async () => {
+        const { context, read, write } = await tk.build({
+            project: 'rebuild',
+            tmp: true,
+        });
+        const css1 = read('dist/index.js');
+        expect(css1, 'initial color').to.includes('color: red');
+        await context.watch();
+        await sleep(2222);
+        write('a.st.css', `.root{color: green}`);
+        await sleep(2222);
+        const css2 = read('dist/index.js');
+        expect(css2, 'color after change').to.includes('color: green');
+    });
+});
