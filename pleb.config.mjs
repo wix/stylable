1e160e8c9ae101df73b6557fe4a311a0106cfc1c
Ido Rosenthal <idoros@gmail.com>
2023-09-18 12:04:09 +0300

    chore(deps): upgrade resolve-directory-context

--- pleb.config.mjs
+++ pleb.config.mjs
@@ -12,9 +12,5 @@ export default {
       name: '@typescript-eslint/utils',
       reason: 'drop node 14 and type issues with ESLintUtils.RuleCreator',
     },
-    {
-      name: '@wixc3/resolve-directory-context',
-      reason: 'drop node 14',
-    },
   ],
 };
