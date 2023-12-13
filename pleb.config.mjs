adab3b28de377524857bca688a8bdab955c09917
Barak <baraki@wix.com>
2023-12-13 11:25:58 +0200

    refactor: remove unused config

--- pleb.config.mjs
+++ pleb.config.mjs
@@ -1,17 +1,5 @@
 export default {
   pinnedPackages: [
-    // {
-    //   name: '@typescript-eslint/eslint-plugin',
-    //   reason: 'drop node 14 and type issues with ESLintUtils.RuleCreator',
-    // },
-    // {
-    //   name: '@typescript-eslint/parser',
-    //   reason: 'drop node 14 and type issues with ESLintUtils.RuleCreator',
-    // },
-    // {
-    //   name: '@typescript-eslint/utils',
-    //   reason: 'drop node 14 and type issues with ESLintUtils.RuleCreator',
-    // },
     {
       name: 'flat',
       reason: 'esm only',
