2c441dd17ffaafe93ec1ebc8c01fef69167122dc
barak igal <baraki@wix.com>
2023-12-13 12:54:44 +0200

    chore!: convert eslint plugin to esm (#2930)

--- pleb.config.mjs
+++ pleb.config.mjs
@@ -1,18 +1,6 @@
 export default {
   pinnedPackages: [
     {
-      name: '@typescript-eslint/eslint-plugin',
-      reason: 'drop node 14 and type issues with ESLintUtils.RuleCreator',
-    },
-    {
-      name: '@typescript-eslint/parser',
-      reason: 'drop node 14 and type issues with ESLintUtils.RuleCreator',
-    },
-    {
-      name: '@typescript-eslint/utils',
-      reason: 'drop node 14 and type issues with ESLintUtils.RuleCreator',
-    },
-    {
       name: 'flat',
       reason: 'esm only',
     },
