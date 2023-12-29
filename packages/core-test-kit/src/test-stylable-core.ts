9a33a7ea20ff14a8424d5ffcc67091645d17ca9a
barak igal <baraki@wix.com>
2023-12-29 11:41:46 +0200

    refactor: 10x better types for test kit. (#2931)

--- packages/core-test-kit/src/test-stylable-core.ts
+++ packages/core-test-kit/src/test-stylable-core.ts
@@ -5,7 +5,7 @@ import type { IDirectoryContents, IFileSystem } from '@file-services/types';
 import { isAbsolute } from 'path';
 
 export interface TestOptions {
-    entries: string[];
+    entries: `/${string}`[];
     stylableConfig: TestStylableConfig;
 }
 
@@ -18,6 +18,9 @@ export type TestStylableConfig = Omit<
     resolveNamespace?: StylableConfig['resolveNamespace'];
 };
 
+type AddLeadingSlash<T extends string> = T extends `/${infer U}` ? `/${U}` : `/${T}`;
+type RemoveRelative<T extends string> = T extends `./${infer U}` ? U : T;
+
 /**
  * The test function takes in a single '/entry.st.css' stylesheet string
  * or a directory structure and then runs 2 phases
@@ -25,10 +28,10 @@ export type TestStylableConfig = Omit<
  * 2. run inline test on all '.st.css' files found
  * @param input single '/entry.st.css' string or file system structure
  */
-export function testStylableCore(
-    input: string | IDirectoryContents,
-    options: Partial<TestOptions> = {}
-) {
+export function testStylableCore<
+    const T extends string | IDirectoryContents,
+    const O extends Partial<TestOptions>
+>(input: T, options: O = {} as O) {
     // infra
     const fs =
         options.stylableConfig?.filesystem ||
@@ -46,14 +49,19 @@ export function testStylableCore(
     const entries = options.entries || allSheets;
 
     // transform entries - run build in requested order
-    const sheets: Record<string, StylableResults> = {};
+    type Entries = O['entries'] extends string[] ? O['entries'][number] : string;
+    type HasExternalFS = O['stylableConfig'] extends { filesystem: IFileSystem } ? true : false;
+    type InputFsKeys = T extends string ? '/entry.st.css' : keyof T;
+    type Keys = HasExternalFS extends true ? string : InputFsKeys;
+    type Sheets = Record<AddLeadingSlash<RemoveRelative<Keys & Entries>>, StylableResults>;
+    const sheets = {} as Sheets;
     for (const path of entries) {
         if (!isAbsolute(path || '')) {
             throw new Error(testStylableCore.errors.absoluteEntry(path));
         }
         const meta = stylable.analyze(path);
         const { exports } = stylable.transform(meta);
-        sheets[path] = { meta, exports };
+        sheets[path as keyof Sheets] = { meta, exports };
     }
 
     // inline test - build all and test
