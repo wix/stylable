11a53037138c6f4aff909483bd120bfe5c9c015c
Ido Rosenthal <idoros@gmail.com>
2024-01-01 13:10:12 +0200

    chore(deps): upgrade to latest stable
    - set tsconfig skipLibCheck to prevent playwirght types issues
    - add chai/mime/postcss-safe-parser to pleb ignore list

--- pleb.config.mjs
+++ pleb.config.mjs
@@ -24,5 +24,17 @@ export default {
       name: 'flat',
       reason: 'esm only',
     },
+    {
+      name: 'chai',
+      reason: 'version 5 is esm only',
+    },
+    {
+      name: 'postcss-safe-parser',
+      reason: 'version 7 removed support for node 16/14/12',
+    },
+    {
+      name: 'mime',
+      reason: 'version 4 is esm only & removed support for node < 18',
+    },
   ],
 };
